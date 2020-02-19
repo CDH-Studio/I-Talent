eee9b98b7f92879c3b3798f860796dd29734d178
Rizvi <rizvida1@gmail.com>
2020-02-19 16:11:42 -0500

    Fixed order of pages

--- services/frontend-v3/src/pages/Profile.jsx
+++ services/frontend-v3/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography } from "antd";
 import SideLayout from "../components/layout/SiderLayout";
 
-const { Paragraph } = Typography;
+const { Title } = Typography;
 
 class Profile extends React.Component {
   goto = link => this.props.history.push(link);
@@ -10,21 +10,14 @@ class Profile extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { str: localStorage.getItem("name") };
+    this.state = { name: localStorage.getItem("name") };
     document.title = this.state.name + " | UpSkill";
   }
-
-  onChange = str => {
-    console.log("Content change:", str);
-    this.setState({ str });
-  };
 
   render() {
     return (
       <SideLayout>
-        <Paragraph editable={{ onChange: this.onChange }}>
-          {this.state.str}
-        </Paragraph>
+        <Title>{localStorage.getItem("name")}</Title>
       </SideLayout>
     );
   }
--- services/frontend-v3/src/pages/ProfileEdit.jsx
+++ services/frontend-v3/src/pages/ProfileEdit.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography } from "antd";
 import SideLayout from "../components/layout/SiderLayout";
 
-const { Title } = Typography;
+const { Paragraph } = Typography;
 
 class Profile extends React.Component {
   goto = link => this.props.history.push(link);
@@ -10,14 +10,21 @@ class Profile extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { name: localStorage.getItem("name") };
+    this.state = { str: localStorage.getItem("name") };
     document.title = this.state.name + " | UpSkill";
   }
+
+  onChange = str => {
+    console.log("Content change:", str);
+    this.setState({ str });
+  };
 
   render() {
     return (
       <SideLayout>
-        <Title>{localStorage.getItem("name")}</Title>
+        <Paragraph editable={{ onChange: this.onChange }}>
+          {this.state.str}
+        </Paragraph>
       </SideLayout>
     );
   }
