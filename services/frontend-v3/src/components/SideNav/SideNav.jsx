3ff87787c3d09492ee250442d833dcde83a7ba57
dynamic11 <dynamic11@gmail.com>
2020-02-19 18:19:25 -0500

    moved side nav into component

---
+++ services/frontend-v3/src/components/SideNav/SideNav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "antd";
+
+import SideNavView from "./SideNavView";
+
+export default class SideNav extends React.Component {
+  render() {
+    return <SideNavView></SideNavView>;
+  }
+}
--- services/frontend-v3/src/components/layout/SiderLayoutView.jsx
+++ services/frontend-v3/src/components/layout/SiderLayoutView.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Affix, Layout, Menu, Icon } from "antd";
 
 import TopNav from "../TopNav/TopNav";
+import SideNav from "../SideNav/SideNav";
 
 const { Header, Sider, Content } = Layout;
 
@@ -23,30 +24,8 @@ export default class SiderLayoutView extends Component {
   render() {
     return (
       <Layout style={{ minHeight: "100vh" }}>
-        <Affix offsetTop={this.state.top}>
-          <Sider
-            trigger={null}
-            collapsible
-            collapsed={this.state.collapsed}
-            style={{ minHeight: "100vh" }}
-          >
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-              <Menu.Item key="1">
-                <Icon type="user" />
-                <span>nav 1s</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="video-camera" />
-                <span>nav 2s</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="upload" />
-                <span>nav 3</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-        </Affix>
+        <SideNav></SideNav>
+
         <Layout>
           <TopNav></TopNav>
           <Content
