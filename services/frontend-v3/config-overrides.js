d6e9a15b0898ffdf6d8912fbc6f79eeaf738478f
dynamic11 <dynamic11@gmail.com>
2020-02-20 02:20:19 -0500

    added color themeing options

--- services/frontend-v3/config-overrides.js
+++ services/frontend-v3/config-overrides.js
@@ -1,4 +1,13 @@
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config;
-};
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { "@primary-color": "#1DA57A" }
+  })
+);
