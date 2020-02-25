47dd9759dd4e8c7a62bbdfc6549006b70f673012
Sagal Maxamud <31967843+s-glmxmd@users.noreply.github.com>
2020-02-25 11:49:13 -0500

    [FIX] Backend config, database and package changes
    Backend was not starting. Had squashed the file changes made to dev so backend was not working.
    Backend functional now.

--- services/backend/config/config.js
+++ services/backend/config/config.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 
-const str = process.env.DATABASE_URL;
-
-let [dialect, host, port] = str.split(":");
-host = host.replace("//", "");
+host = process.env.PGHOST;
 
 module.exports = {
   development: {
@@ -11,16 +8,16 @@ module.exports = {
     password: process.env.PGPASS,
     database: process.env.PGDATABASE,
     host: host,
-    port: port,
-    dialect: dialect
+    port: 5432,
+    dialect: "postgres"
   },
   production: {
     username: process.env.PGUSERNAME,
     password: process.env.PGPASS,
     database: process.env.PGDATABASE,
     host: host,
-    port: port,
-    dialect: dialect
+    port: 5432,
+    dialect: "postgres"
   },
   staging: {
     uri: process.env.DATABASE_URL,
--- services/backend/config/database.js
+++ services/backend/config/database.js
@@ -2,21 +2,15 @@
 const Sequelize = require("sequelize");
 
 require("dotenv").config();
-
-const str = process.env.DATABASE_URL;
-
-let [dialect, host, port] = str.split(":");
-host = host.replace("//", "");
-
 // Option 1: Passing parameters separately
 module.exports = new Sequelize(
   process.env.PGDATABASE,
   process.env.PGUSERNAME,
   process.env.PGPASS,
   {
-    host: host,
-    port: port,
-    dialect: dialect,
+    host: process.env.PGHOST,
+    port: 5432,
+    dialect: "postgres",
     pool: {
       max: 5,
       min: 0,
