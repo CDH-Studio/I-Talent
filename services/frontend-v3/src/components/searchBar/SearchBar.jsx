e4c4e680fcf2892629d2dd4b8e0ece0f38e8ef29
KateAThornley <kate.yelnroth@gmail.com>
2020-02-25 08:47:09 -0500

    All search field values stored in one array

--- services/frontend-v3/src/components/searchBar/SearchBar.jsx
+++ services/frontend-v3/src/components/searchBar/SearchBar.jsx
@@ -1,27 +1,19 @@
 import React from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
-import queryString from "query-string";
+
 import {} from "antd";
 
 import SearchBarView from "./SearchBarView";
 
-export default class SearchBar extends React.Component {
+class SearchBar extends React.Component {
   render() {
-    const { name, data, avatar, locale } = this.props;
     return (
       <SearchBarView>
-        changeLanguage={this.props.changeLanguage}
+        changeLanguage=
+        {this.props.changeLanguage}
         keycloak={this.props.keycloak}
         history={this.props.history}
       </SearchBarView>
     );
   }
 }
-// const backendLink =
-//   "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/";
-// function fieldNamesToArray() {
-//   const fieldNames = async () => {
-//     const response = await fetch(backendLink + "api/option/");
-//   };
-// }
+export default SearchBar;
