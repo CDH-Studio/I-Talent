b89e12b864e840a9e9fd7350de52483c11849051
dynamic11 <dynamic11@gmail.com>
2020-02-26 14:43:09 -0500

    clean up profile create

--- services/frontend-v3/src/pages/ProfileCreate.jsx
+++ services/frontend-v3/src/pages/ProfileCreate.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Typography, Steps } from "antd";
 import CreateProfileLayout from "../components/layouts/createProfileLayout/CreateProfileLayout";
 
 class ProfileCreate extends React.Component {
@@ -9,23 +8,9 @@ class ProfileCreate extends React.Component {
     super(props);
 
     document.title = "Create Profile | UpSkill";
-
-    this.state = {
-      current: 0
-    };
   }
 
-  onChange = current => {
-    console.log("onChange:", current);
-    this.setState({ current });
-  };
-
   render() {
-    // const { current } = this.state;
-    // let form = this.profileFormSelect(1);
-    //let stepz = this.props.match.params.step;
-    //console.log(stepz);
-
     return (
       <CreateProfileLayout
         changeLanguage={this.props.changeLanguage}
@@ -38,5 +23,4 @@ class ProfileCreate extends React.Component {
   }
 }
 
-//Needed when using this,props.intl
 export default ProfileCreate;
