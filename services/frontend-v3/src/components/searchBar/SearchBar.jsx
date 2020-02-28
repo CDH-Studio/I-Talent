4920b73eddf72c469ea70b3f2afd71b518276f6e
KateAThornley <kate.yelnroth@gmail.com>
2020-02-28 10:58:44 -0500

    Moved Controller functions from View into Controller

--- services/frontend-v3/src/components/searchBar/SearchBar.jsx
+++ services/frontend-v3/src/components/searchBar/SearchBar.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import prepareInfo from "../../functions/prepareInfo";
+import { Form, Col, Input, Switch, Select } from "antd";
 import axios from "axios";
 import {} from "antd";
 import config from "../../config";
-
+import queryString from "query-string";
+import { injectIntl } from "react-intl";
 import SearchBarView from "./SearchBarView";
 const backendAddress = config.backendAddress;
+const { Option } = Select;
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      expand: false,
       advancedOptions: null
     };
     this.getAdvancedOptions = this.getAdvancedOptions.bind(this);
+    this.getFields = this.getFields.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.getBasicField = this.getBasicField.bind(this);
   }
+
+  toggle = () => {
+    const { expand } = this.state;
+    this.setState({ expand: !expand });
+  };
 
   async getAdvancedOptions() {
     const lang = localStorage.getItem("lang");
@@ -51,7 +64,100 @@ class SearchBar extends React.Component {
           value: obj.description.en
         }))
     };
+    this.setState({ advancedOptions: advancedOptions });
   }
+
+  getBasicField(data) {
+    const children = [];
+    const searchLabel = this.props.intl.formatMessage({
+      id: "button.search",
+      defaultMessage: "Search"
+    });
+    const { getFieldDecorator } = this.props.form;
+    children.push(
+      <Form.Item label={"Search"}>
+        {getFieldDecorator("Search", {})(<Input placeholder={searchLabel} />)}
+      </Form.Item>
+    );
+    return children;
+  }
+
+  getFields(data) {
+    const count = this.state.expand ? 6 : 0;
+    const { advancedOptions } = this.props;
+    const { getFieldDecorator } = this.props.form;
+    const children = [];
+    var fieldCounter = 0;
+    const searchLabel = this.props.intl.formatMessage({
+      id: "button.search",
+      defaultMessage: "Search"
+    });
+    const labelArr = [
+      this.props.intl.formatMessage({
+        id: "advanced.search.form.name",
+        defaultMessage: "Name"
+      }),
+      this.props.intl.formatMessage({
+        id: "advanced.search.form.skills",
+        defaultMessage: "Skills"
+      }),
+      this.props.intl.formatMessage({
+        id: "advanced.search.form.branch",
+        defaultMessage: "Branch"
+      }),
+      this.props.intl.formatMessage({
+        id: "advanced.search.form.location",
+        defaultMessage: "Location"
+      }),
+      this.props.intl.formatMessage({
+        id: "advanced.search.form.classification",
+        defaultMessage: "Classification"
+      }),
+      this.props.intl.formatMessage({
+        id: "advanced.search.form.ex.feeder",
+        defaultMessage: "Ex Feeder"
+      })
+    ];
+    for (let i = 0; i < 10; i++) {
+      fieldCounter++;
+      children.push(
+        <Col span={8} key={i} style={{ display: i < count ? "block" : "none" }}>
+          {fieldCounter !== 6 ? (
+            <Form.Item label={labelArr[i]}>
+              {getFieldDecorator(
+                "" + labelArr[i],
+                {}
+              )(<Input placeholder={searchLabel} />)}
+            </Form.Item>
+          ) : (
+            <Form.Item style={{ textAlign: "center" }} label={labelArr[i]}>
+              {getFieldDecorator("Switch", { valuePropName: "checked" })(
+                <Switch />
+              )}
+            </Form.Item>
+          )}
+        </Col>
+      );
+    }
+    return children;
+  }
+
+  handleSearch = e => {
+    console.log("");
+    var query;
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      query = queryString.stringify(values, { arrayFormat: "bracket" });
+
+      let url = "/secured/results?" + encodeURI(query);
+
+      this.props.history.push(url);
+    });
+  };
+
+  handleReset = () => {
+    this.props.form.resetFields();
+  };
 
   render() {
     return (
@@ -60,8 +166,15 @@ class SearchBar extends React.Component {
         keycloak={this.props.keycloak}
         history={this.props.history}
         getAdvancedOptions={this.getAdvancedOptions}
+        advancedOptions={this.state.advancedOptions}
+        getFields={this.getFields}
+        handleReset={this.handleReset}
+        handleSearch={this.handleSearch}
+        toggle={this.toggle}
+        getBasicField={this.getBasicField}
       ></SearchBarView>
     );
   }
 }
-export default SearchBar;
+SearchBar = Form.create({})(SearchBar);
+export default injectIntl(SearchBar);
--- services/frontend-v3/src/components/searchBar/SearchBarView.jsx
+++ services/frontend-v3/src/components/searchBar/SearchBarView.jsx
@@ -1,163 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Form, Row, Col, Input, Button, Icon, Switch, Card } from "antd";
-import prepareInfo from "../../functions/prepareInfo";
-import axios from "axios";
-import config from "../../config";
-import queryString from "query-string";
+import { injectIntl } from "react-intl";
+import { Form, Row, Select, Col, Button, Icon, Card } from "antd";
+
 import logo from "../sideNav/logo_v2.svg";
 
-import { injectIntl } from "react-intl";
-const backendAddress = config.backendAddress;
+const { Option } = Select;
 class SearchBarView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       expand: false
-      //   advancedOptions: null
     };
-    //this.getAdvancedOptions = this.getAdvancedOptions.bind(this);
   }
-
-  //   async getAdvancedOptions() {
-  //     const lang = localStorage.getItem("lang");
-  //     let advancedOptions = {
-  //       classification: prepareInfo(
-  //         (await axios.get(backendAddress + "api/option/getGroupLevel")).data,
-  //         lang
-  //       ).map(obj => ({
-  //         key: obj.id,
-  //         text: obj.description,
-  //         value: obj.id
-  //       })),
-  //       skills: prepareInfo(
-  //         (await axios.get(backendAddress + "api/option/getDevelopmentalGoals"))
-  //           .data,
-  //         lang
-  //       ).map(obj => ({
-  //         key: obj.id,
-  //         text: obj.description,
-  //         value: obj.id
-  //       })),
-  //       location: prepareInfo(
-  //         (await axios.get(backendAddress + "api/option/getLocation")).data,
-  //         lang
-  //       ).map(obj => ({
-  //         key: obj.id,
-  //         text: obj.description,
-  //         value: obj.id
-  //       })),
-  //       branch: (await axios.get(backendAddress + "api/option/getBranch")).data
-  //         .filter(elem => elem.description && elem.description.en)
-  //         .map(obj => ({
-  //           key: obj.description.en,
-  //           text: obj.description[lang],
-  //           value: obj.description.en
-  //         }))
-  //     };
-  //   }
-
-  getFields(data) {
-    const count = this.state.expand ? 6 : 0;
-    const { advancedOptions } = this.props;
-    const { getFieldDecorator } = this.props.form;
-    const children = [];
-    var fieldCounter = 0;
-    const searchLabel = this.props.intl.formatMessage({
-      id: "button.search",
-      defaultMessage: "Search"
-    });
-    const labelArr = [
-      this.props.intl.formatMessage({
-        id: "advanced.search.form.name",
-        defaultMessage: "Name"
-      }),
-      this.props.intl.formatMessage({
-        id: "advanced.search.form.skills",
-        defaultMessage: "Skills"
-      }),
-      this.props.intl.formatMessage({
-        id: "advanced.search.form.branch",
-        defaultMessage: "Branch"
-      }),
-      this.props.intl.formatMessage({
-        id: "advanced.search.form.location",
-        defaultMessage: "Location"
-      }),
-      this.props.intl.formatMessage({
-        id: "advanced.search.form.classification",
-        defaultMessage: "Classification"
-      }),
-      this.props.intl.formatMessage({
-        id: "advanced.search.form.ex.feeder",
-        defaultMessage: "Ex Feeder"
-      })
-    ];
-    for (let i = 0; i < 10; i++) {
-      fieldCounter++;
-      children.push(
-        <Col span={8} key={i} style={{ display: i < count ? "block" : "none" }}>
-          {fieldCounter !== 6 ? (
-            <Form.Item
-              //options={advancedOptions.classification}
-              label={labelArr[i]}
-            >
-              {getFieldDecorator(
-                "" + labelArr[i],
-                {}
-              )(<Input placeholder={searchLabel} />)}
-            </Form.Item>
-          ) : (
-            <Form.Item style={{ textAlign: "center" }} label={labelArr[i]}>
-              {getFieldDecorator("Switch", { valuePropName: "checked" })(
-                <Switch />
-              )}
-            </Form.Item>
-          )}
-        </Col>
-      );
-    }
-    return children;
-  }
-
-  handleSearch = e => {
-    var query;
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      var basicSearch = values[0];
-      var searchName = values[1];
-      var searchSkills = values[2];
-      var searchBranch = values[3];
-      var searchLocation = values[4];
-      var searchClassification = values[5];
-      var searchSwitch = values[6];
-      query = queryString.stringify(values, { arrayFormat: "bracket" });
-
-      let url = "/secured/results?" + encodeURI(query);
-
-      this.props.history.push(url);
-    });
-  };
-
-  handleReset = () => {
-    this.props.form.resetFields();
-  };
-
-  toggle = () => {
-    const { expand } = this.state;
-    this.setState({ expand: !expand });
-  };
 
   render() {
+    const {
+      advancedOptions,
+      getFields,
+      getBasicField,
+      handleSearch,
+      handleReset,
+      toggle
+    } = this.props;
     const { data } = this.props;
-    const { getFieldDecorator } = this.props.form;
+
     const searchLabel = this.props.intl.formatMessage({
       id: "button.search",
       defaultMessage: "Search"
     });
 
     return (
-      <Form onSubmit={this.handleSearch}>
+      <Form onSubmit={handleSearch}>
         <div
           style={{
             paddingTop: "80px",
@@ -185,12 +58,7 @@ class SearchBarView extends React.Component {
             >
               <img src={logo} alt="UpSkill Logo" style={{ height: "80px" }} />;
             </header>
-            <div style={{ paddingBottom: "20px" }}>
-              {getFieldDecorator(
-                "Search",
-                {}
-              )(<Input placeholder={searchLabel} />)}
-            </div>
+            <div style={{ paddingBottom: "20px" }}>{getBasicField(data)}</div>
             <Col span={24} style={{ textAlign: "right", paddingTop: "0px" }}>
               <Button
                 shape="round"
@@ -205,7 +73,7 @@ class SearchBarView extends React.Component {
                 shape="round"
                 size="large"
                 style={{ marginLeft: 8 }}
-                onClick={this.handleReset}
+                onClick={handleReset}
               >
                 {this.props.intl.formatMessage({
                   id: "button.clear",
@@ -217,13 +85,10 @@ class SearchBarView extends React.Component {
           <Card
             style={{ boxShadow: "5px 5px 5px #e6e6e6", borderRadius: "5px" }}
           >
-            <Row gutter={24}>{this.getFields(data)}</Row>
+            <Row gutter={24}>{getFields(data)}</Row>
             <Row>
               <Col span={24} style={{ textAlign: "right" }}>
-                <a
-                  style={{ marginLeft: 8, fontSize: 14 }}
-                  onClick={this.toggle}
-                >
+                <a style={{ marginLeft: 8, fontSize: 14 }} onClick={toggle}>
                   {this.props.intl.formatMessage({
                     id: "advanced.search.button.text",
                     defaultMessage: "Advanced Search"
@@ -238,6 +103,4 @@ class SearchBarView extends React.Component {
     );
   }
 }
-
-SearchBarView = Form.create({})(SearchBarView);
 export default injectIntl(SearchBarView);
