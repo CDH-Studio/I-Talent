3508a487095d779f670ad52c3b91ab15dfb8f6f2
Mamadou Bah <bmama067@uottawa.ca>
2020-03-03 16:25:26 -0500

    clean up

--- services/frontend-v3/src/config.js
+++ services/frontend-v3/src/config.js
@@ -2,7 +2,6 @@ export default {
   backendAddressLoc: "http://localhost:8080/",
   backendAddressDev:
     "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/",
-  backendAddress:
-    // "http://localhost:8080/"
-    "http://upskill-backend-uat-upskill.apps.dev.openshift.ised-isde.canada.ca/"
+  backendAddress: "http://localhost:8080/"
+    //"http://upskill-backend-uat-upskill.apps.dev.openshift.ised-isde.canada.ca/"
 };
