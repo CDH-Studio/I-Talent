747d8d99bd093324400eddb6a3d261b888260408
KateAThornley <kate.yelnroth@gmail.com>
2020-03-03 08:29:28 -0500

    fixed broken language change on results page

--- services/frontend-v3/src/pages/Results.jsx
+++ services/frontend-v3/src/pages/Results.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Row, Button, Menu, Icon } from "antd";
 import AppLayout from "../components/layouts/appLayout/AppLayout";
 
 class Results extends React.Component {
@@ -12,7 +12,31 @@ class Results extends React.Component {
   }
 
   render() {
-    return <AppLayout>TEST!!!</AppLayout>;
+    const sideBarContent = (
+      <Menu theme="light" mode="inline" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1">
+          <Icon type="user" />
+          <span>nav 1s</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="video-camera" />
+          <span>nav 2s</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="upload" />
+          <span>nav 3</span>
+        </Menu.Item>
+      </Menu>
+    );
+    return (
+      <AppLayout
+        changeLanguage={this.props.changeLanguage}
+        keycloak={this.props.keycloak}
+        history={this.props.history}
+        displaySideBar={true}
+        sideBarContent={sideBarContent}
+      ></AppLayout>
+    );
   }
 }
 
