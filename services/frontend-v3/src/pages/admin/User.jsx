e8667805185680afb3dc414d445eb06f4f2c32b0
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-03-04 09:46:00 -0500

    [New] Added new features to table
    - Dropdown with fields
    - Apply button
    - Link button for each profile
    - Able to sort columns

--- services/frontend-v3/src/pages/admin/User.jsx
+++ services/frontend-v3/src/pages/admin/User.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import AppLayout from "../../components/layouts/appLayout/AppLayout";
 import axios from "axios";
-import { Typography, Skeleton, Icon, Table, Input, Button } from "antd";
+import {
+  Typography,
+  Skeleton,
+  Icon,
+  Table,
+  Input,
+  Button,
+  Row,
+  Col,
+  Form,
+  Select
+} from "antd";
 import _ from "lodash";
 import moment from "moment";
 import { injectIntl } from "react-intl";
@@ -11,6 +22,8 @@ import config from "../../config";
 const backendAddress = config.backendAddress;
 
 const { Title } = Typography;
+
+const { Option } = Select;
 
 class AdminUser extends React.Component {
   goto = link => this.props.history.push(link);
@@ -30,7 +43,8 @@ class AdminUser extends React.Component {
       loading: true,
       activePage: 1,
       searchText: "",
-      searchedColumn: ""
+      searchedColumn: "",
+      size: "large"
     };
   }
 
@@ -131,8 +145,91 @@ class AdminUser extends React.Component {
     this.setState({ searchText: "" });
   };
 
+  handleDropdownChange = (status, id) => {
+    this.setState(({ statuses, allData }) => {
+      statuses[id] = status;
+
+      let changedUser = _.remove(allData, user => user.id === id);
+
+      changedUser = changedUser[0];
+
+      if (status === "Active") {
+        changedUser.flagged = false;
+        changedUser.user.inactive = false;
+      }
+
+      if (status === "Inactive") {
+        changedUser.flagged = false;
+        changedUser.user.inactive = true;
+      }
+
+      if (status === "Hidden") {
+        changedUser.flagged = true;
+        changedUser.user.inactive = false;
+      }
+
+      allData.push(changedUser);
+
+      return { statuses, allData };
+    });
+  };
+
+  profileStatusValue = (inactive, flagged) => {
+    if (inactive)
+      return this.props.intl.formatMessage({
+        id: "admin.inactive",
+        defaultMessage: "Inactive"
+      });
+    else if (flagged)
+      return this.props.intl.formatMessage({
+        id: "admin.flagged",
+        defaultMessage: "Hidden"
+      });
+    return this.props.intl.formatMessage({
+      id: "admin.active",
+      defaultMessage: "Active"
+    });
+  };
+
+  renderStatusDropdown = (id, inactive, flagged) => {
+    const options = [
+      { value: "Active", text: "Active", key: "active" },
+      { value: "Inactive", text: "Inactive", key: "inactive" },
+      { value: "Hidden", text: "Hidden", key: "flagged" }
+    ];
+
+    return (
+      <div>
+        <Select
+          defaultValue={this.profileStatusValue(inactive, flagged)}
+          style={{ width: 120 }}
+          // onChange={}
+        >
+          <Option key="active">
+            {this.props.intl.formatMessage({
+              id: "admin.active",
+              defaultMessage: "Active"
+            })}
+          </Option>
+          <Option key="inactive">
+            {this.props.intl.formatMessage({
+              id: "admin.inactive",
+              defaultMessage: "Inactive"
+            })}
+          </Option>
+          <Option key="hidden">
+            {this.props.intl.formatMessage({
+              id: "admin.flagged",
+              defaultMessage: "Hidden"
+            })}
+          </Option>
+        </Select>
+      </div>
+    );
+  };
+
   render() {
-    const { data, loading } = this.state;
+    const { data, loading, size } = this.state;
     if (loading) {
       return (
         <AppLayout>
@@ -146,6 +243,7 @@ class AdminUser extends React.Component {
     allUsers.map(function(e) {
       e.fullName = e.user.name;
       e.formatCreatedAt = moment(e.createdAt).format("LLL");
+      e.profileLink = "/secured/profile/" + e.id;
       if (e.tenure === null) {
         e.tenureDescriptionEn = "None Specified";
         e.tenureDescriptionFr = "Aucun spécifié";
@@ -155,9 +253,35 @@ class AdminUser extends React.Component {
       }
     });
 
-    // console.log(allUsers);
+    const jobTitleState =
+      this.props.intl.formatMessage({ id: "language.code" }) === "en"
+        ? "jobTitleEn"
+        : "jobTitleFr";
+
+    const tenureState =
+      this.props.intl.formatMessage({ id: "language.code" }) === "en"
+        ? "tenureDescriptionEn"
+        : "tenureDescriptionFr";
+
+    console.log(allUsers);
 
     const user_table_columns = [
+      {
+        title: this.props.intl.formatMessage({
+          id: "admin.view",
+          defaultMessage: "View"
+        }),
+        render: (text, record) => (
+          <span>
+            <Button
+              type="primary"
+              shape="circle"
+              icon="link"
+              onClick={() => window.open(record.profileLink)}
+            />
+          </span>
+        )
+      },
       {
         title: this.props.intl.formatMessage({
           id: "admin.name",
@@ -165,6 +289,10 @@ class AdminUser extends React.Component {
         }),
         dataIndex: "fullName",
         key: "name",
+        sorter: (a, b) => {
+          return a.fullName.localeCompare(b.fullName);
+        },
+        sortDirections: ["descend"],
         ...this.getColumnSearchProps("fullName")
       },
       {
@@ -172,16 +300,12 @@ class AdminUser extends React.Component {
           id: "admin.job.title",
           defaultMessage: "Job Title"
         }),
-        dataIndex:
-          this.props.intl.formatMessage({ id: "language.code" }) === "en"
-            ? "jobTitleEn"
-            : "jobTitleFr",
+        dataIndex: jobTitleState,
         key: "jobTitle",
-        ...this.getColumnSearchProps(
-          this.props.intl.formatMessage({ id: "language.code" }) === "en"
-            ? "jobTitleEn"
-            : "jobTitleFr"
-        )
+        sorter: (a, b) => {
+          return a[jobTitleState].localeCompare(b[jobTitleState]);
+        },
+        ...this.getColumnSearchProps(jobTitleState)
       },
       {
         title: this.props.intl.formatMessage({
@@ -190,6 +314,11 @@ class AdminUser extends React.Component {
         }),
         dataIndex: "formatCreatedAt",
         key: "registered",
+        sorter: (a, b) => {
+          return (
+            moment(a.formatCreatedAt).unix() - moment(b.formatCreatedAt).unix()
+          );
+        },
         ...this.getColumnSearchProps("formatCreatedAt")
       },
       {
@@ -197,16 +326,25 @@ class AdminUser extends React.Component {
           id: "admin.tenure",
           defaultMessage: "Tenure"
         }),
-        dataIndex:
-          this.props.intl.formatMessage({ id: "language.code" }) === "en"
-            ? "tenureDescriptionEn"
-            : "tenureDescriptionFr",
+        dataIndex: tenureState,
         key: "tenure",
-        ...this.getColumnSearchProps(
-          this.props.intl.formatMessage({ id: "language.code" }) === "en"
-            ? "tenureDescriptionEn"
-            : "tenureDescriptionFr"
-        )
+        sorter: (a, b) => {
+          return a[tenureState].localeCompare(b[tenureState]);
+        },
+        ...this.getColumnSearchProps(tenureState)
+      },
+      {
+        title: this.props.intl.formatMessage({
+          id: "admin.profileStatus",
+          defaultMessage: "Profile Status"
+        }),
+        render: record => {
+          return this.renderStatusDropdown(
+            record.id,
+            record.user.inactive,
+            record.flagged
+          );
+        }
       }
     ];
 
@@ -218,6 +356,19 @@ class AdminUser extends React.Component {
         displaySideBar={true}
       >
         <Title>Users</Title>
+        <div align="right">
+          <Button
+            type="primary"
+            icon="check-circle"
+            size={size}
+            //onClick={() => window.open(record.profileLink)}
+          >
+            {this.props.intl.formatMessage({
+              id: "admin.apply",
+              defaultMessage: "Apply"
+            })}
+          </Button>
+        </div>
         <Table columns={user_table_columns} dataSource={allUsers} />
       </AppLayout>
     );
