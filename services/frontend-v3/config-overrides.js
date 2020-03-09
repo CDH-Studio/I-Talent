471771e8f32e1eeaa64187ab05bee280fec073c3
Ali Nouri <dynamic11@gmail.com>
2020-03-09 13:55:53 -0400

    temp fix for backend

--- services/frontend-v3/config-overrides.js
+++ services/frontend-v3/config-overrides.js
@@ -10,7 +10,6 @@ module.exports = override(
     javascriptEnabled: true,
     modifyVars: {
       "@primary-color": "#007471",
-      "@light-primary-color": "#a1b3af"
       "@link": "#00B9B2",
       "@success-color": "#00cc00",
       "@warning-color": "#ff661a",
