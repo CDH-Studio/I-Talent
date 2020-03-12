7a69a8805ccb887fc807cfeaec00d06a1a1527cf
Rizvi <rizvida1@gmail.com>
2020-03-12 13:53:34 -0400

    Update config.js

--- services/frontend-v3/src/config.js
+++ services/frontend-v3/src/config.js
@@ -1,8 +1,3 @@
 export default {
-  backendAddressLoc: "http://localhost:8080/",
-  backendAddressDev:
-    "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/",
-  backendAddressUat:
-    "http://upskill-backend-uat-upskill.apps.dev.openshift.ised-isde.canada.ca/",
   backendAddress: process.env.REACT_APP_API_ADDRESS
 };
