c1117d84597363bc5963c7065cce884c3b0997f6
dynamic11 <dynamic11@gmail.com>
2020-03-13 10:18:16 -0400

    added toggle to employment form

---
+++ services/frontend-v3/src/components/formLabelTooltip/FormLabelTooltipView.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Tooltip } from "antd";
+
+import { InfoCircleOutlined } from "@ant-design/icons";
+
+function FormLabelTooltipView(props) {
+  /* Component Styles */
+  const styles = {
+    tooltipIcon: {
+      color: "rgba(0,0,0,.45)",
+      marginLeft: "5px"
+    }
+  };
+
+  return (
+    <div style={{ display: "inline", marginRight: "10px" }}>
+      {props.labelText}
+      <Tooltip title={props.tooltipText}>
+        <InfoCircleOutlined style={styles.tooltipIcon} />
+      </Tooltip>
+    </div>
+  );
+}
+
+export default FormLabelTooltipView;
