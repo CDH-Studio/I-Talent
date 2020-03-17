faeab1bbb721b374a305a13ad961b8a1ad0b456a
dynamic11 <dynamic11@gmail.com>
2020-03-17 01:20:27 -0400

    moved some functions into useeffect

--- services/frontend-v3/src/components/profileForms/employmentDataForm/EmploymentDataForm.jsx
+++ services/frontend-v3/src/components/profileForms/employmentDataForm/EmploymentDataForm.jsx
@@ -16,70 +16,70 @@ function EmploymentDataForm() {
   const [profileInfo, setProfileInfo] = useState(null);
   const [load, setLoad] = useState(false);
 
-  /* get substantive level options */
-  const getSubstantiveOptions = async () => {
-    try {
-      let result = await axios.get(backendAddress + "api/option/getTenure");
-      await setSubstantiveOptions(result.data);
-      return 1;
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
-
-  /* get classification options */
-  const getClassificationOptions = async () => {
-    try {
-      let url = backendAddress + "api/option/getGroupLevel";
-      let result = await axios.get(url);
-      return await setClassificationOptions(result.data);
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
-
-  /* get security options */
-  const getSecurityOptions = async () => {
-    try {
-      let url = backendAddress + "api/option/getSecurityClearance";
-      let result = await axios.get(url);
-      return await setSecurityOptions(result.data);
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
-
-  /* get user profile for form drop down */
-  const getProfileInfo = async () => {
-    try {
-      let url =
-        backendAddress + "api/profile/" + localStorage.getItem("userId");
-      let result = await axios.get(url);
-      await setProfileInfo(result.data);
-      return 1;
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
-
-  /* get all required data component */
-  const getAllData = async () => {
-    try {
-      await getClassificationOptions();
-      await getSubstantiveOptions();
-      await getSecurityOptions();
-      await getProfileInfo();
-      setLoad(true);
-      return 1;
-    } catch (error) {
-      setLoad(false);
-      console.log(error);
-      return 0;
-    }
-  };
-
   /* useEffect to run once component is mounted */
   useEffect(() => {
+    /* get substantive level options */
+    const getSubstantiveOptions = async () => {
+      try {
+        let result = await axios.get(backendAddress + "api/option/getTenure");
+        await setSubstantiveOptions(result.data);
+        return 1;
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    /* get classification options */
+    const getClassificationOptions = async () => {
+      try {
+        let url = backendAddress + "api/option/getGroupLevel";
+        let result = await axios.get(url);
+        return await setClassificationOptions(result.data);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    /* get security options */
+    const getSecurityOptions = async () => {
+      try {
+        let url = backendAddress + "api/option/getSecurityClearance";
+        let result = await axios.get(url);
+        return await setSecurityOptions(result.data);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    /* get user profile for form drop down */
+    const getProfileInfo = async () => {
+      try {
+        let url =
+          backendAddress + "api/profile/" + localStorage.getItem("userId");
+        let result = await axios.get(url);
+        await setProfileInfo(result.data);
+        return 1;
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    /* get all required data component */
+    const getAllData = async () => {
+      try {
+        await getClassificationOptions();
+        await getSubstantiveOptions();
+        await getSecurityOptions();
+        await getProfileInfo();
+        setLoad(true);
+        return 1;
+      } catch (error) {
+        setLoad(false);
+        console.log(error);
+        return 0;
+      }
+    };
+
     getAllData();
   }, []);
 
