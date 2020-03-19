0a1545f357db80854ab4573017af403d1d9f2704
Rizvi Rab <31704878+Rizvida1@users.noreply.github.com>
2020-03-19 09:49:57 -0400

    [New] Added support for OpenShift Deployment (#53)
    - Now app can be deployed to OpenShift
    
    **Note: You must have web.env in /env**
    Copy the web.env.example and rename it to web.env

--- services/frontend-v3/src/config.js
+++ services/frontend-v3/src/config.js
@@ -1,7 +1,3 @@
 export default {
-  backendAddressLoc: "http://localhost:8080/",
-  backendAddressDev:
-    "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/",
-  backendAddress: "http://localhost:8080/"
-  //"http://upskill-backend-uat-upskill.apps.dev.openshift.ised-isde.canada.ca/"
+  backendAddress: process.env.REACT_APP_API_ADDRESS
 };
