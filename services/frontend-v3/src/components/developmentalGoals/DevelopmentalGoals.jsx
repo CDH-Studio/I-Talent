dffa2207694eae0087eebd52b2ef2ec1238dd7da
Sagal Maxamud <smaxa028@uottawa.ca>
2020-03-31 15:51:27 -0400

    DevGoals and DevGoalsView converted to functional component
    Tested and functional! :smile:

--- services/frontend-v3/src/components/developmentalGoals/DevelopmentalGoals.jsx
+++ services/frontend-v3/src/components/developmentalGoals/DevelopmentalGoals.jsx
@@ -3,10 +3,10 @@ import { injectIntl } from "react-intl";
 
 import DevelopmentalGoalsView from "./DevelopmentalGoalsView";
 
-class DevelopmentalGoals extends Component {
-  formatData() {
-    const { data } = this.props;
-    const locale = this.props.intl.formatMessage({ id: "language.code" });
+function DevelopmentalGoals(props) {
+  const formatData = dataSource => {
+    const data = dataSource.data;
+    const locale = dataSource.intl.formatMessage({ id: "language.code" });
 
     let devGoals = {};
     let key = 0;
@@ -17,11 +17,9 @@ class DevelopmentalGoals extends Component {
     });
 
     return devGoals;
-  }
+  };
 
-  render() {
-    return <DevelopmentalGoalsView devGoals={this.formatData()} />;
-  }
+  return <DevelopmentalGoalsView devGoals={formatData(props)} />;
 }
 
 export default injectIntl(DevelopmentalGoals);
--- services/frontend-v3/src/components/developmentalGoals/DevelopmentalGoalsView.jsx
+++ services/frontend-v3/src/components/developmentalGoals/DevelopmentalGoalsView.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import { List, Tag, Row } from "antd";
 
-class DevelopmentalGoalsView extends Component {
-  render() {
-    const { devGoals } = this.props;
-    return (
-      <div>
-        <Row>
-          <List>
-            {Object.values(devGoals).map(devGoal => (
-              <Tag style={{ marginBottom: "8px", marginTop: "8px" }}>
-                {devGoal}
-              </Tag>
-            ))}
-          </List>
-        </Row>
-      </div>
-    );
-  }
+function DevelopmentalGoalsView(props) {
+  const devGoals = props.devGoals;
+  return (
+    <>
+      <Row>
+        <List>
+          {Object.values(devGoals).map(devGoal => (
+            <Tag style={{ marginBottom: "8px", marginTop: "8px" }}>
+              {devGoal}
+            </Tag>
+          ))}
+        </List>
+      </Row>
+    </>
+  );
 }
 
 export default DevelopmentalGoalsView;
