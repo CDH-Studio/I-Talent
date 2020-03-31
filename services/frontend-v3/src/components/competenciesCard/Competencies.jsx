385105e102f1a56a5a6ef93254e8b87f37cab0aa
Sagal Maxamud <smaxa028@uottawa.ca>
2020-03-31 14:56:44 -0400

    Competencies and CompetenciesView converted to functional components
    Tested and functional :smile:

--- services/frontend-v3/src/components/competenciesCard/Competencies.jsx
+++ services/frontend-v3/src/components/competenciesCard/Competencies.jsx
@@ -3,10 +3,10 @@ import { injectIntl } from "react-intl";
 
 import CompetenciesView from "./CompetenciesView";
 
-class Competencies extends Component {
-  formatData() {
-    const { data } = this.props;
-    const locale = this.props.intl.formatMessage({ id: "language.code" });
+function Competencies(props) {
+  const formatData = dataSource => {
+    const data = dataSource.data;
+    const locale = dataSource.intl.formatMessage({ id: "language.code" });
 
     let competencies = {};
     let key = 0;
@@ -17,11 +17,9 @@ class Competencies extends Component {
     });
 
     return competencies;
-  }
+  };
 
-  render() {
-    return <CompetenciesView competencies={this.formatData()} />;
-  }
+  return <CompetenciesView competencies={formatData(props)} />;
 }
 
 export default injectIntl(Competencies);
--- services/frontend-v3/src/components/competenciesCard/CompetenciesView.jsx
+++ services/frontend-v3/src/components/competenciesCard/CompetenciesView.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import { List, Tag, Row } from "antd";
 
-class CompetenciesView extends Component {
-  render() {
-    const { competencies } = this.props;
-    return (
-      <div>
-        <Row>
-          <List>
-            {Object.values(competencies).map(competency => (
-              <Tag style={{ marginBottom: "8px", marginTop: "8px" }}>
-                {competency}
-              </Tag>
-            ))}
-          </List>
-        </Row>
-      </div>
-    );
-  }
+function CompetenciesView(props) {
+  const competencies = props.competencies;
+  return (
+    <>
+      <Row>
+        <List>
+          {Object.values(competencies).map(competency => (
+            <Tag style={{ marginBottom: "8px", marginTop: "8px" }}>
+              {competency}
+            </Tag>
+          ))}
+        </List>
+      </Row>
+    </>
+  );
 }
 
 export default CompetenciesView;
