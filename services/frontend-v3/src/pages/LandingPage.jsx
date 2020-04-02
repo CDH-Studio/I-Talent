aee44c48e0f8dfa9f1174f56df4875f0120cfa1d
KateAThornley <ktott56789@gmail.com>
2020-04-02 12:49:55 -0400

    moved styling to const styles

--- services/frontend-v3/src/pages/LandingPage.jsx
+++ services/frontend-v3/src/pages/LandingPage.jsx
@@ -7,9 +7,6 @@ import { injectIntl } from "react-intl";
 
 /** UI for the landing route layout */
 function LandingPage(props) {
-  //   const propTypes = {
-  //     changeLanguage: propTypes.func.isRequired
-  //   };
   const { changeLanguage } = props;
   const handleClick = e => {
     console.log("click ", e);
@@ -18,58 +15,27 @@ function LandingPage(props) {
   return (
     <Row>
       <Menu
-        style={{ backgroundColor: "#001C1A", width: "100%" }}
+        style={styles.menuStyle}
         onClick={handleClick}
         fixed="top"
         mode="horizontal"
       >
-        <Menu.Item style={{ paddingBottom: "8px", paddingTop: "8px" }}>
-          <img src={Logo} style={{ maxWidth: "37px" }} alt="UpSkill Logo" />
+        <Menu.Item style={styles.logoStyle}>
+          <img src={Logo} style={styles.imgStyle} alt="UpSkill Logo" />
         </Menu.Item>
-        <Menu.Item
-          style={{
-            paddingBottom: "6px",
-            paddingTop: "8px",
-            marginLeft: "86%"
-          }}
-        >
+        <Menu.Item style={styles.bttnStyle}>
           <ChangeLanguage changeLanguage={changeLanguage} />
         </Menu.Item>
       </Menu>
       <React.Fragment>
-        <div
-          style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            height: "100%",
-            position: "fixed",
-            top: "62px",
-            width: "100%"
-          }}
-        >
-          <div
-            style={{
-              color: "#EFEEEE",
-              fontSize: "52pt",
-              margin: "250px auto 0px",
-              textAlign: "center",
-              width: "100%",
-
-              fontWeight: "bold"
-            }}
-          >
+        <div style={styles.backgroundStyle}>
+          <div style={styles.mainMessage}>
             {props.intl.formatMessage({
               id: "landing.welcome",
               defaultMessage: "Welcome to MyTalent"
             })}
           </div>
-          <div
-            style={{
-              width: "100%",
-              textAlign: "center"
-            }}
-          >
+          <div style={styles.signIn}>
             <Button
               style={{ margin: "0px", paddingTop: "0px", fontSize: "20px" }}
               as="a"
@@ -89,4 +55,41 @@ function LandingPage(props) {
     </Row>
   );
 }
+const styles = {
+  menuStyle: {
+    backgroundColor: "#001C1A",
+    width: "100%"
+  },
+  logoStyle: {
+    paddingBottom: "8px",
+    paddingTop: "8px"
+  },
+  imgStyle: { maxWidth: "37px" },
+  bttnStyle: {
+    paddingBottom: "6px",
+    paddingTop: "8px",
+    marginLeft: "86%"
+  },
+  backgroundStyle: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    position: "fixed",
+    top: "62px",
+    width: "100%"
+  },
+  mainMessage: {
+    color: "#EFEEEE",
+    fontSize: "52pt",
+    margin: "250px auto 0px",
+    textAlign: "center",
+    width: "100%",
+    fontWeight: "bold"
+  },
+  signIn: {
+    width: "100%",
+    textAlign: "center"
+  }
+};
 export default injectIntl(LandingPage);
