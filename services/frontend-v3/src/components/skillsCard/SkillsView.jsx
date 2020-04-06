1955794445bfec22b318bf425be8013b60118da9
Ali Nouri <dynamic11@gmail.com>
2020-04-06 04:11:19 -0400

    display frindly message if data for tags is empty

--- services/frontend-v3/src/components/skillsCard/SkillsView.jsx
+++ services/frontend-v3/src/components/skillsCard/SkillsView.jsx
@@ -1,38 +1,48 @@
 import React from "react";
-import { Card, Tag, Row, Col, Collapse } from "antd";
+import { Tag, Collapse, Empty } from "antd";
+
 import { TagTwoTone } from "@ant-design/icons";
 
 const { Panel } = Collapse;
 
 function SkillsView(props) {
-  const styles = {
-    cards: {
-      borderWidth: "medium",
-    },
+  /*
+   * Generate Skills Collapse
+   *
+   * Generate a collapsible menu organized by skill category
+   * If no skills are found for the profile then display friendly message
+   */
+  const generateSkillsCollapse = (categoriesSkills, skills) => {
+    if (skills.length > 0) {
+      return (
+        <Collapse>
+          {categoriesSkills.map(
+            (categorySkill, index) =>
+              categorySkill != null && (
+                <Panel
+                  header={categorySkill.val}
+                  key={index + 1}
+                  extra={<TagTwoTone twoToneColor="#3CBAB3" />}
+                >
+                  {skills[categorySkill.index].val.map((skill) => (
+                    <Tag color="#007471">{skill}</Tag>
+                  ))}
+                </Panel>
+              )
+          )}
+        </Collapse>
+      );
+    } else {
+      return (
+        <Empty
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+          description="no skills provided"
+        />
+      );
+    }
   };
-  const skills = props.skills;
-  const categoriesSkills = props.categoriesSkills;
 
-  return (
-    <>
-      <Collapse>
-        {categoriesSkills.map(
-          (categorySkill, index) =>
-            categorySkill != null && (
-              <Panel
-                header={categorySkill.val}
-                key={index + 1}
-                extra={<TagTwoTone twoToneColor="#3CBAB3" />}
-              >
-                {skills[categorySkill.index].val.map((skill) => (
-                  <Tag color="#007471">{skill}</Tag>
-                ))}
-              </Panel>
-            )
-        )}
-      </Collapse>
-    </>
-  );
+  return generateSkillsCollapse(props.categoriesSkills, props.skills);
 }
 
 export default SkillsView;
