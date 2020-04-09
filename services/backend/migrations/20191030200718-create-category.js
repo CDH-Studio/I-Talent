cc878d148cb207113a5028e8b1c3474a3cb45f58
Sagal Maxamud <31967843+s-glmxmd@users.noreply.github.com>
2020-04-09 15:19:02 -0400

    Limiting duplicate values in skills and categories tables (#97)
    Adding constraint on english and french description so that users cannot enter values that are already present in the table
    _Changes to admin frontend modals need to be made to reflect these restrictions from backend_

--- services/backend/migrations/20191030200718-create-category.js
+++ services/backend/migrations/20191030200718-create-category.js
@@ -1,31 +1,47 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('categories', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true // initial value 0.. skills id is set to the associated number
-        // need to figure out if 1) important to have 
-      },
-      descriptionEn: {
-        type: Sequelize.STRING
-      },
-      descriptionFr: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.createTable(
+        "categories",
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true, // initial value 0.. skills id is set to the associated number
+            // need to figure out if 1) important to have
+          },
+          descriptionEn: {
+            type: Sequelize.STRING,
+          },
+          descriptionFr: {
+            type: Sequelize.STRING,
+          },
+          createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+          },
+          updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+          },
+        },
+        transaction
+      );
+      // admin user cannot enter a category description if the combination of french and english text already exists in the table
+      await queryInterface.addConstraint(
+        "categories",
+        ["descriptionEn", "descriptionFr"],
+        {
+          type: "unique",
+          name: "categories_unique_definition",
+          transaction,
+        }
+      );
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('categories');
-  }
+    return queryInterface.dropTable("categories");
+  },
 };
