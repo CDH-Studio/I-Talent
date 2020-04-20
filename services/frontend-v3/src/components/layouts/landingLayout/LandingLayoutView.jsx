d3aa894b5d1bbcf2f0a8ef02d0b4ea2cf650a53e
Ali Nouri <dynamic11@gmail.com>
2020-04-19 21:54:47 -0400

    added landing page content

--- services/frontend-v3/src/components/layouts/landingLayout/LandingLayoutView.jsx
+++ services/frontend-v3/src/components/layouts/landingLayout/LandingLayoutView.jsx
@@ -1,19 +1,73 @@
 import React from "react";
 import AppLayout from "../appLayout/AppLayout";
-import { Row, Col } from "antd";
+import { Row, Col, Typography, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { FormattedMessage } from "react-intl";
+import backgroundOptionOne from "../../../assets/landing-1.svg";
+import backgroundOptionTwo from "../../../assets/landing-2.svg";
+import backgroundOptionThree from "../../../assets/landing-3.svg";
+
+const { Text, Title } = Typography;
 
 const LandingLayoutView = (props) => {
+  const randomPictureSelect = () => {
+    const imageOptions = [
+      backgroundOptionOne,
+      backgroundOptionTwo,
+      backgroundOptionThree,
+    ];
+    let randomIndex = Math.floor(Math.random() * imageOptions.length);
+    return imageOptions[randomIndex];
+  };
+
   return (
     <AppLayout
       changeLanguage={props.changeLanguage}
       keycloak={props.keycloak}
       displaySideBar={false}
     >
-      <Row justify="center" class={{ minHeight: "100vh" }}>
-        <Col span={4}>col-4</Col>
-        <Col span={4}>col-4</Col>
-        <Col span={4}>col-4</Col>
-        <Col span={4}>col-4</Col>
+      <Row justify="center" style={{ marginTop: "150px" }}>
+        <Col span={6} style={{ baddingTop: "60px" }}>
+          <img
+            src={require("../../../assets/MyTalent-Logo-Full-v2-dark.svg")}
+            alt="Logo"
+            style={{ width: "250px", marginTop: "30px" }}
+          />
+          <Title
+            level={1}
+            style={{
+              display: "block",
+              margin: "25px 0 10px 0",
+              color: "#404040",
+            }}
+          >
+            Welcome
+          </Title>
+          <Text
+            style={{ display: "block", margin: "20px 0", fontSize: "15px" }}
+            strong
+          >
+            I-Talent is a platform to showcase your skills and talent within
+            ISED! Connect with other talented individuals to learn and grow
+            together.
+          </Text>
+          <Text
+            style={{ display: "block", margin: "20px 0", fontSize: "15px" }}
+            strong
+          >
+            Connect with other talented individuals to learn and grow together.
+          </Text>
+          <Button type="primary" href={"/secured/home"} size={"large"}>
+            <FormattedMessage id="landing.login.button" />
+          </Button>
+        </Col>
+        <Col span={10} style={{ textAlign: "right" }}>
+          <img
+            src={randomPictureSelect()}
+            alt="Logo"
+            style={{ maxWidth: "70%", maxHeight: "400px" }}
+          />
+        </Col>
       </Row>
     </AppLayout>
   );
