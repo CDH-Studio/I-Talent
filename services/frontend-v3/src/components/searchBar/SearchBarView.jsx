dd2a8965836401b43ae280b229dd8c505f880abd
Ali Nouri <dynamic11@gmail.com>
2020-05-07 14:48:06 -0400

    made advanced search expandable again

--- services/frontend-v3/src/components/searchBar/SearchBarView.jsx
+++ services/frontend-v3/src/components/searchBar/SearchBarView.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import { injectIntl, FormattedMessage } from "react-intl";
-import { Row, Col, Button, Form, Alert, Input, Switch, Select } from "antd";
 import {
-  SearchOutlined,
-  SettingOutlined,
-  DownOutlined,
-  UpOutlined,
-} from "@ant-design/icons";
+  Typography,
+  Row,
+  Col,
+  Button,
+  Form,
+  Alert,
+  Input,
+  Switch,
+  Select,
+} from "antd";
+import { SearchOutlined, SettingOutlined } from "@ant-design/icons";
 import "@ant-design/compatible/assets/index.css";
 import logo from "../../assets/MyTalent-Logo-Full-v2.svg";
 
 const { Option } = Select;
+const { Title } = Typography;
 
 function SearchBarView(props) {
+  const [expand, setExpand] = useState(false);
   const [doNothing, setDoNothing] = useState(true);
   const [form] = Form.useForm();
-  const { getFields, handleSearch, expand, toggle, data, empty } = props;
+  const { handleSearch, data, empty } = props;
 
   const styles = {
     outerForm: {
@@ -74,6 +81,10 @@ function SearchBarView(props) {
     defaultMessage: "Search",
   });
 
+  const toggle = () => {
+    setExpand(!expand);
+  };
+
   // Generate the basic input field for basic search
   const getBasicField = () => {
     return (
@@ -85,117 +96,144 @@ function SearchBarView(props) {
 
   // Generate the advanced search fields
   const getAdvancedSearchForm = () => {
+    // detect language
     let locale = props.intl.formatMessage({
       id: "language.code",
       defaultMessage: "en",
     });
 
-    return (
-      <Row>
-        {/* form column one */}
-        <Col md={8}>
-          {/* name field */}
-          <Form.Item
-            label={<FormattedMessage id="advanced.search.form.name" />}
-            name="name"
-          >
-            <Input style={{ width: "80%" }} placeholder={searchLabel} />
-          </Form.Item>
-          {/* Location field */}
-          <Form.Item
-            label={<FormattedMessage id="advanced.search.form.location" />}
-            name={"location"}
-          >
-            <Select
-              style={{ width: "80% " }}
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              mode="multiple"
-              placeholder={searchLabel}
-            >
-              {props.locationOptions.map((value) => {
-                return (
-                  <Option key={value.id}>{value.description[locale]}</Option>
-                );
-              })}
-            </Select>
-          </Form.Item>
-        </Col>
-        {/* form column two */}
-        <Col md={8}>
-          {/* Skills field */}
-          <Form.Item
-            label={<FormattedMessage id="advanced.search.form.skills" />}
-            name={"skills"}
-          >
-            <Select
-              style={{ width: "80%" }}
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              mode="multiple"
-              placeholder={searchLabel}
-            >
-              {props.skillOptions.map((value) => {
-                return (
-                  <Option key={value.id}>{value.description[locale]}</Option>
-                );
-              })}
-            </Select>
-          </Form.Item>
-          {/* classification field */}
-          <Form.Item
-            label={
-              <FormattedMessage id="advanced.search.form.classification" />
-            }
-            name={"classification"}
-          >
-            <Select
-              style={{ width: "80%" }}
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              mode="multiple"
-              placeholder={searchLabel}
-            >
-              {props.classOptions.map((value) => {
-                return <Option key={value.id}>{value.description}</Option>;
-              })}
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col md={8}>
-          <Form.Item
-            label={<FormattedMessage id="advanced.search.form.branch" />}
-            name={"branch"}
-          >
-            <Select
-              style={{ width: "80%" }}
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              mode="multiple"
-              placeholder={searchLabel}
-            >
-              {props.branchOptions.map((value) => {
-                return (
-                  <Option key={value.description.en}>
-                    {value.description[locale]}
-                  </Option>
-                );
-              })}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            name={<FormattedMessage id="advanced.search.form.ex.feeder" />}
-            label={"exFeeder"}
-          >
-            <Switch />
-          </Form.Item>
-        </Col>
-      </Row>
-    );
+    if (!expand) {
+      return <div />;
+    } else {
+      return (
+        <div style={{ marginBottom: "0" }}>
+          <Row style={{ padding: "20px 5% 5px 5%" }}>
+            <Col span={24} style={{ padding: "0px 0" }}>
+              <Title level={2} style={{ fontSize: "1.3em" }}>
+                <FormattedMessage id={"advanced.search.button.text"} />
+              </Title>
+            </Col>
+          </Row>
+
+          <Row gutter={[48, 24]} style={{ padding: "0px 5%" }}>
+            {/* form column one */}
+            <Col span={8}>
+              {/* name field */}
+              <Form.Item
+                label={<FormattedMessage id="advanced.search.form.name" />}
+                name="name"
+              >
+                <Input style={{ width: "100%" }} placeholder={searchLabel} />
+              </Form.Item>
+              {/* Location field */}
+              <Form.Item
+                label={<FormattedMessage id="advanced.search.form.location" />}
+                name={"location"}
+              >
+                <Select
+                  style={{ width: "100% " }}
+                  filterOption={(input, option) =>
+                    option.children
+                      .toLowerCase()
+                      .indexOf(input.toLowerCase()) >= 0
+                  }
+                  mode="multiple"
+                  placeholder={searchLabel}
+                >
+                  {props.locationOptions.map((value) => {
+                    return (
+                      <Option key={value.id}>
+                        {value.description[locale]}
+                      </Option>
+                    );
+                  })}
+                </Select>
+              </Form.Item>
+            </Col>
+            {/* form column two */}
+            <Col span={8}>
+              {/* Skills field */}
+              <Form.Item
+                label={<FormattedMessage id="advanced.search.form.skills" />}
+                name={"skills"}
+              >
+                <Select
+                  style={{ width: "100%" }}
+                  filterOption={(input, option) =>
+                    option.children
+                      .toLowerCase()
+                      .indexOf(input.toLowerCase()) >= 0
+                  }
+                  mode="multiple"
+                  placeholder={searchLabel}
+                >
+                  {props.skillOptions.map((value) => {
+                    return (
+                      <Option key={value.id}>
+                        {value.description[locale]}
+                      </Option>
+                    );
+                  })}
+                </Select>
+              </Form.Item>
+              {/* classification field */}
+              <Form.Item
+                label={
+                  <FormattedMessage id="advanced.search.form.classification" />
+                }
+                name={"classification"}
+              >
+                <Select
+                  style={{ width: "100%" }}
+                  filterOption={(input, option) =>
+                    option.children
+                      .toLowerCase()
+                      .indexOf(input.toLowerCase()) >= 0
+                  }
+                  mode="multiple"
+                  placeholder={searchLabel}
+                >
+                  {props.classOptions.map((value) => {
+                    return <Option key={value.id}>{value.description}</Option>;
+                  })}
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item
+                label={<FormattedMessage id="advanced.search.form.branch" />}
+                name={"branch"}
+              >
+                <Select
+                  style={{ width: "100%" }}
+                  filterOption={(input, option) =>
+                    option.children
+                      .toLowerCase()
+                      .indexOf(input.toLowerCase()) >= 0
+                  }
+                  mode="multiple"
+                  placeholder={searchLabel}
+                >
+                  {props.branchOptions.map((value) => {
+                    return (
+                      <Option key={value.description.en}>
+                        {value.description[locale]}
+                      </Option>
+                    );
+                  })}
+                </Select>
+              </Form.Item>
+              <Form.Item
+                name={<FormattedMessage id="advanced.search.form.ex.feeder" />}
+                label={"exFeeder"}
+              >
+                <Switch />
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
   };
 
   const onFinish = (values) => {
@@ -257,8 +295,6 @@ function SearchBarView(props) {
         </div>
         <div style={styles.advSearchCard}>
           {/* Gets fields for Advanced Search in collapse */}
-
-          {getFields(data)}
           {getAdvancedSearchForm()}
           <Row>
             <Col span={24} style={styles.advFieldPlacement}>
