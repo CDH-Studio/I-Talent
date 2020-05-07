16d044a22e517d64e50b9d504f164640a9aea5be
Mohamed Radwan <21133129+MohamedRadwan@users.noreply.github.com>
2020-05-07 15:57:20 -0400

    remove frontend v1 and v2

--- services/frontend/src/components/editForms/competenciesForm/competenciesFormController.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import CompetenciesFormView from "./competenciesFormView";
-
-export default class CompeneciesFormController extends Component {
-  render() {
-    return <CompetenciesFormView {...this.props} />;
-  }
-}
--- services/frontend/src/components/editForms/educationForm/educationFormController.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import EducationFormView from "./educationFormView";
-
-export default class EducationFormController extends Component {
-  render() {
-    return <EducationFormView {...this.props} />;
-  }
-}
