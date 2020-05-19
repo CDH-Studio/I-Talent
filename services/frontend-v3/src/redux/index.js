6d05abf4a3f3ab630a758a8336fb989e60cb0560
Benoit Jeaurond <benoit@jeaurond.dev>
2020-05-19 14:35:21 -0400

    Fix redux-persist serializableCheck

--- services/frontend-v3/src/redux/index.js
+++ services/frontend-v3/src/redux/index.js
@@ -1,5 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./slices";
 
@@ -10,6 +19,11 @@ const persistConfig = {
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, rootReducer),
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export const persistor = persistStore(store);
