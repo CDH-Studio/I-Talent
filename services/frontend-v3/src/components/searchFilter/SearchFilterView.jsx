33f70c4b9fbb850c6e730ec4bf46c3e0d5958ef2
Ali Nouri <dynamic11@gmail.com>
2020-05-19 17:16:49 -0400

    restyle search button and fix mobile view

--- services/frontend-v3/src/components/searchFilter/SearchFilterView.jsx
+++ services/frontend-v3/src/components/searchFilter/SearchFilterView.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { injectIntl } from "react-intl";
 import { Form, Col, Button, Input, Switch, Select, Row } from "antd";
+import { ReloadOutlined } from "@ant-design/icons";
 import "@ant-design/compatible/assets/index.css";
 import { useSelector } from "react-redux";
 import { IntlPropType, IdDescriptionPropType } from "../../customPropTypes";
@@ -75,131 +76,94 @@ const SearchBarView = ({
     }),
   ];
   return (
-    <row>
-      <Col span={24}>
-        <Form
-          style={{ padding: "22px" }}
-          form={form}
-          layout="vertical"
-          onFinish={onFinish}
+    <Form
+      style={{ padding: "22px" }}
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+    >
+      <Form.Item label={labelArr[0]} name={searchTitles[0]} style={styles.w100}>
+        <Input style={styles.w100} placeholder={searchLabel} />
+      </Form.Item>
+      <Form.Item style={styles.w100} label={labelArr[1]} name={searchTitles[1]}>
+        <Select
+          style={styles.w100}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          mode="multiple"
+          placeholder={searchLabel}
         >
-          <Row>
-            <Form.Item
-              label={labelArr[0]}
-              name={searchTitles[0]}
-              style={styles.w100}
-            >
-              <Input style={styles.w100} placeholder={searchLabel} />
-            </Form.Item>
-            <Form.Item
-              style={styles.w100}
-              label={labelArr[1]}
-              name={searchTitles[1]}
-            >
-              <Select
-                style={styles.w100}
-                filterOption={(input, option) =>
-                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
-                  0
-                }
-                mode="multiple"
-                placeholder={searchLabel}
-              >
-                {skillOptions.map((value) => {
-                  return (
-                    <Option key={value.id}>{value.description[locale]}</Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-            <Form.Item
-              style={styles.w100}
-              label={labelArr[2]}
-              name={searchTitles[2]}
-            >
-              <Select
-                style={styles.w100}
-                filterOption={(input, option) =>
-                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
-                  0
-                }
-                mode="multiple"
-                placeholder={searchLabel}
-              >
-                {branchOptions.map((value) => {
-                  return (
-                    <Option key={value.description.en}>
-                      {value.description[locale]}
-                    </Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-            <Form.Item
-              style={styles.w100}
-              label={labelArr[3]}
-              name={searchTitles[3]}
-            >
-              <Select
-                style={styles.w100}
-                filterOption={(input, option) =>
-                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
-                  0
-                }
-                mode="multiple"
-                placeholder={searchLabel}
-              >
-                {locationOptions.map((value) => {
-                  return (
-                    <Option key={value.id}>{value.description[locale]}</Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-            <Form.Item
-              style={styles.w100}
-              label={labelArr[4]}
-              name={searchTitles[4]}
-            >
-              <Select
-                style={styles.w100}
-                filterOption={(input, option) =>
-                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
-                  0
-                }
-                mode="multiple"
-                placeholder={searchLabel}
-              >
-                {classOptions.map((value) => {
-                  return <Option key={value.id}>{value.description}</Option>;
-                })}
-              </Select>
-            </Form.Item>
-            <Form.Item
-              style={styles.w100}
-              name={searchTitles[5]}
-              label={labelArr[5]}
-              valuePropName="checked"
-            >
-              <Switch />
-            </Form.Item>{" "}
-          </Row>
-
-          <Row style={{ justifyContent: "center" }}>
-            <Col>
-              <Button
-                shape="round"
-                size="large"
-                type="primary"
-                htmlType="submit"
-              >
-                {searchLabel}
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-      </Col>
-    </row>
+          {skillOptions.map((value) => {
+            return <Option key={value.id}>{value.description[locale]}</Option>;
+          })}
+        </Select>
+      </Form.Item>
+      <Form.Item style={styles.w100} label={labelArr[2]} name={searchTitles[2]}>
+        <Select
+          style={styles.w100}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          mode="multiple"
+          placeholder={searchLabel}
+        >
+          {branchOptions.map((value) => {
+            return (
+              <Option key={value.description.en}>
+                {value.description[locale]}
+              </Option>
+            );
+          })}
+        </Select>
+      </Form.Item>
+      <Form.Item style={styles.w100} label={labelArr[3]} name={searchTitles[3]}>
+        <Select
+          style={styles.w100}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          mode="multiple"
+          placeholder={searchLabel}
+        >
+          {locationOptions.map((value) => {
+            return <Option key={value.id}>{value.description[locale]}</Option>;
+          })}
+        </Select>
+      </Form.Item>
+      <Form.Item style={styles.w100} label={labelArr[4]} name={searchTitles[4]}>
+        <Select
+          style={styles.w100}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          mode="multiple"
+          placeholder={searchLabel}
+        >
+          {classOptions.map((value) => {
+            return <Option key={value.id}>{value.description}</Option>;
+          })}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        style={styles.w100}
+        name={searchTitles[5]}
+        label={labelArr[5]}
+        valuePropName="checked"
+      >
+        <Switch />
+      </Form.Item>{" "}
+      <Button
+        style={styles.w100}
+        shape="round"
+        size="large"
+        icon={<ReloadOutlined />}
+        type="primary"
+        htmlType="submit"
+      >
+        {searchLabel}
+      </Button>
+    </Form>
   );
 };
 
