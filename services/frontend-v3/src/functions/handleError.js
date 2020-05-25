582997a634849c262839b57d4be15ae2d7d223b3
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2020-05-25 15:13:12 -0400

    small cleanup to handleError

--- services/frontend-v3/src/functions/handleError.js
+++ services/frontend-v3/src/functions/handleError.js
@@ -1,12 +1,14 @@
 import { message } from "antd";
 import { IntlProvider } from "react-intl";
+
 import { addError } from "../redux/slices/errorsSlice";
 import history from "../history";
 import store from "../redux";
+
 import enIntlMessages from "../i18n/en_CA.json";
 import frIntlMessages from "../i18n/fr_CA.json";
+
 import config from "../config";
-import FormLabelTooltip from "../components/formLabelTooltip/FormLabelTooltip";
 const { enableErrorRedirect } = config;
 
 const errorMessages = {
@@ -14,14 +16,9 @@ const errorMessages = {
   fr: frIntlMessages["error.save"] || "error.save",
 };
 
-export default (error, performReduxDispatch, handleType) => {
-  if (performReduxDispatch) {
-    console.log(performReduxDispatch);
-    console.log(" err", errorMessages);
-    console.log(store);
-    store.dispatch(addError(error));
-  }
+export default (error, handleType) => {
   if (handleType === "redirect" && enableErrorRedirect) {
+    store.dispatch(addError(error));
     history.push("/error");
   } else if (handleType === "message") {
     message.error(errorMessages[store.getState().settings.locale]);
