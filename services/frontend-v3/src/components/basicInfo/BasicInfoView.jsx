6ab097bca8bab06a65b8a656d2b8f6e350e70d28
Benoit Jeaurond <benoit@jeaurond.dev>
2020-06-02 15:05:11 -0400

    Fix profile name overlapping in profile layout

--- services/frontend-v3/src/components/basicInfo/BasicInfoView.jsx
+++ services/frontend-v3/src/components/basicInfo/BasicInfoView.jsx
@@ -51,7 +51,7 @@ const BasicInfoView = ({
   const generateProfileHeader = () => {
     return (
       <Row type="flex" style={styles.profileHeaderRow}>
-        <Col xs={12} md={5} lg={4} xl={3} align="center">
+        <Col xs={12} md={5} lg={4} xxl={3} align="center">
           <Avatar
             size={80}
             style={(styles.userAvatar, { backgroundColor: avatar.color })}
@@ -61,7 +61,7 @@ const BasicInfoView = ({
             </Text>
           </Avatar>
         </Col>
-        <Col xs={12} md={19} lg={20} xl={21} style={{ padding: "11px 10px" }}>
+        <Col xs={12} md={19} lg={20} xxl={21} style={{ padding: "11px 10px" }}>
           <Text
             strong
             style={{ display: "block", fontSize: "30px", lineHeight: "38px" }}
