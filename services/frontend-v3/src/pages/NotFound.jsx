28035bd749475141d68553db1e40be9d713b834d
KateAThornley <ktott56789@gmail.com>
2020-06-02 09:31:14 -0400

    added missing highlight to btn links

--- services/frontend-v3/src/pages/NotFound.jsx
+++ services/frontend-v3/src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import ErrorResult from "../components/errorResult/errorResult";
 const NotFound = () => {
   return (
     <ErrorResult errorCode={404}>
-      <h1 className="hidden">Error Page - Code 400</h1>
+      <h1 className="hidden">Error - Code 400</h1>
     </ErrorResult>
   );
 };
