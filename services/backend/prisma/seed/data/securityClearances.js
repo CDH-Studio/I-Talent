cc6fd396261ef738b2054f0b3d46c3525147a1bf
Benoit Jeaurond <benoit@jeaurond.dev>
2020-06-04 11:52:06 -0400

    Added seeds

---
+++ services/backend/prisma/seed/data/securityClearances.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    descriptionEn: "Reliability",
+    descriptionFr: "Cote de fiabilité",
+  },
+  {
+    descriptionEn: "Secret",
+    descriptionFr: "Secret",
+  },
+  {
+    descriptionEn: "Top Secret",
+    descriptionFr: "Très Secret",
+  },
+];
---
+++ services/backend/prisma/seed/data/tenures.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    descriptionEn: "Term",
+    descriptionFr: "Période",
+  },
+  {
+    descriptionEn: "Casual",
+    descriptionFr: "Temporaire",
+  },
+  {
+    descriptionEn: "Indeterminate",
+    descriptionFr: "Indéterminé",
+  },
+];
