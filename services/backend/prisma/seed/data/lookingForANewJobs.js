e79b852588ed89f9a273ac0984d97771f89fbb0a
Benoit Jeaurond <benoit@jeaurond.dev>
2020-06-04 11:52:06 -0400

    Added seeds

---
+++ services/backend/prisma/seed/data/lookingForANewJobs.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    descriptionEn: "Actively applying",
+    descriptionFr: "Applique activement",
+  },
+  {
+    descriptionEn: "Casually looking",
+    descriptionFr: "Regarde les opportunités mine de rien",
+  },
+  {
+    descriptionEn: "Not looking but open to offers",
+    descriptionFr: "Ne cherche pas mais est ouvert aux offres",
+  },
+  {
+    descriptionEn: "Not looking",
+    descriptionFr: "Ne cherche pas",
+  },
+];
---
+++ services/backend/prisma/seed/data/users.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    name: "John Doe",
+    email: "john.doe@canada.ca",
+  },
+  {
+    id: "faba08aa-ffe3-11e9-8d71-362b9e155667",
+    name: "Mary Doe",
+    email: "mary.doe@canada.ca",
+  },
+];
