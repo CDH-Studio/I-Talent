066c1826de374852bd5f59dc7063359e1f5836a9
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2020-06-10 08:19:39 -0400

    fix eslint issue

--- services/backend/src/database/migrations/20191031145350-create-user-and-profile.js
+++ services/backend/src/database/migrations/20191031145350-create-user-and-profile.js
@@ -152,7 +152,7 @@ module.exports = {
         });
       });
   },
-  down: (queryInterface) => {
+  down: queryInterface => {
     return queryInterface.dropTable("profiles").then(() => {
       return queryInterface.dropTable("users");
     });
