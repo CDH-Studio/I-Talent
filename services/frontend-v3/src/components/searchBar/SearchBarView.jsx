a83c06d11cce7cfeefbf6f7bcb638e1ff2ba4e96
Benoit Jeaurond <benoit@jeaurond.dev>
2020-06-15 16:00:18 -0400

    Fixed home page

--- services/frontend-v3/src/components/searchBar/SearchBarView.jsx
+++ services/frontend-v3/src/components/searchBar/SearchBarView.jsx
@@ -13,7 +13,7 @@ import {
 } from "antd";
 import { SearchOutlined, SettingOutlined } from "@ant-design/icons";
 import logo from "../../assets/MyTalent-Logo-Full-v2.svg";
-import { IntlPropType, IdDescriptionPropType } from "../../customPropTypes";
+import { IntlPropType } from "../../customPropTypes";
 
 const { Option } = Select;
 const { Title } = Typography;
@@ -106,14 +106,8 @@ const SearchBarView = ({
 
   // Generate the advanced search fields
   const getAdvancedSearchForm = (displayForm) => {
-    // detect language
-    const locale = intl.formatMessage({
-      id: "language.code",
-      defaultMessage: "en",
-    });
-
     if (!displayForm) {
-      return <div />;
+      return null;
     }
     return (
       <div style={{ marginBottom: "0" }}>
@@ -143,8 +137,10 @@ const SearchBarView = ({
               <Select
                 style={{ width: "100%" }}
                 filterOption={(input, option) =>
-                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
-                  0
+                  option.children
+                    .join("")
+                    .toLowerCase()
+                    .indexOf(input.toLowerCase()) >= 0
                 }
                 mode="multiple"
                 placeholder={searchLabel}
@@ -152,7 +148,10 @@ const SearchBarView = ({
               >
                 {locationOptions.map((value) => {
                   return (
-                    <Option key={value.id}>{value.description[locale]}</Option>
+                    <Option key={value.id}>
+                      {value.streetNumber} {value.streetName}, {value.city},{" "}
+                      {value.province}
+                    </Option>
                   );
                 })}
               </Select>
@@ -176,9 +175,7 @@ const SearchBarView = ({
                 maxTagCount={3}
               >
                 {skillOptions.map((value) => {
-                  return (
-                    <Option key={value.id}>{value.description[locale]}</Option>
-                  );
+                  return <Option key={value.id}>{value.name}</Option>;
                 })}
               </Select>
             </Form.Item>
@@ -200,7 +197,7 @@ const SearchBarView = ({
                 placeholder={searchLabel}
               >
                 {classOptions.map((value) => {
-                  return <Option key={value.id}>{value.description}</Option>;
+                  return <Option key={value.id}>{value.name}</Option>;
                 })}
               </Select>
             </Form.Item>
@@ -223,11 +220,7 @@ const SearchBarView = ({
                 maxTagCount={3}
               >
                 {branchOptions.map((value) => {
-                  return (
-                    <Option key={value.description.en}>
-                      {value.description[locale]}
-                    </Option>
-                  );
+                  return <Option key={value}>{value}</Option>;
                 })}
               </Select>
             </Form.Item>
@@ -311,22 +304,28 @@ const SearchBarView = ({
 };
 
 SearchBarView.propTypes = {
-  branchOptions: PropTypes.arrayOf(
-    PropTypes.shape({
-      description: PropTypes.shape({
-        en: PropTypes.string,
-        fr: PropTypes.string,
-      }),
-    })
-  ).isRequired,
+  branchOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
   classOptions: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
-      description: PropTypes.string,
+      name: PropTypes.string,
     })
   ).isRequired,
-  locationOptions: IdDescriptionPropType.isRequired,
-  skillOptions: IdDescriptionPropType.isRequired,
+  locationOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      city: PropTypes.string,
+      province: PropTypes.string,
+      streetName: PropTypes.string,
+      streetNumber: PropTypes.number,
+    })
+  ).isRequired,
+  skillOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ).isRequired,
   handleSearch: PropTypes.func.isRequired,
   intl: IntlPropType,
 };
