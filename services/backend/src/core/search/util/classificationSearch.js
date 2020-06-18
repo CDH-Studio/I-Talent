69ef5dfb32b18230bd234e33b20f87fa6097a243
Benoît Jeaurond <benoit@jeaurond.dev>
2020-06-18 13:13:58 -0400

    Fixed search API (#408)

--- services/backend/src/core/search/util/classificationSearch.js
+++ services/backend/src/core/search/util/classificationSearch.js
@@ -2,12 +2,13 @@ async function classificationSearch(profiles, classificationArray) {
   const classificationProf = [];
 
   profiles.forEach((profile) => {
-    if (!profile.classification) return;
-    const classificationId = profile.classification.id;
+    if (!profile.groupLevel) return;
 
-    if (classificationArray.includes(classificationId))
+    if (classificationArray.includes(profile.groupLevel.id)) {
       classificationProf.push(profile);
+    }
   });
+
   return classificationProf;
 }
 
