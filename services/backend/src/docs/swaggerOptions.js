bcff76293ac171c9dad4d3959a4e0dc01eb496ee
Benoit Jeaurond <benoit@jeaurond.dev>
2020-06-24 14:06:49 -0400

    Added swagger custimizations

--- services/backend/src/docs/swaggerOptions.js
+++ services/backend/src/docs/swaggerOptions.js
@@ -6,7 +6,16 @@ const specs = swaggerJsdoc({
     info: {
       title: "I-Talent API",
       version: "1.0.0",
-      description: "Exposed API endpoints for the I-Talent frontend",
+      description:
+        "Exposed [express](https://expressjs.com/) node.js API endpoints for the I-Talent frontend querying a PostgreSQL database with [prisma.io](https://www.prisma.io/). \n\n To query the API, you'll need to authenticate yourself with your ISED account below.",
+      license: {
+        name: "MIT",
+        url: "https://opensource.org/licenses/MIT",
+      },
+      contact: {
+        name: "GitHub",
+        url: "https://github.com/CDH-Studio/UpSkill",
+      },
     },
     securityDefinitions: {
       Keycloak: {
@@ -23,10 +32,15 @@ const specs = swaggerJsdoc({
 module.exports = swaggerUi.setup(specs, {
   customSiteTitle: "I-Talent API",
   customfavIcon: "http://localhost:3000/favicon.ico",
+  customCss:
+    ".swagger-ui .topbar { display: none } .swagger-ui .scheme-container { background: none; box-shadow: none }",
   swaggerOptions: {
     oauth: {
       clientId: process.env.DOCS_KEYCLOAK_CLIENT_ID,
       clientSecret: process.env.KEYCLOAK_SECRET,
     },
+    defaultModelsExpandDepth: -1,
+    filter: true,
+    withCredentials: true,
   },
 });
