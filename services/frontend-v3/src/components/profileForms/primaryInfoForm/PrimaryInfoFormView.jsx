bb6f1a38c3a3ce5196b01bcb10ae499edeca4cd7
KateAThornley <ktott56789@gmail.com>
2020-06-29 21:52:28 -0400

    added tooltip for updating GEDS info

--- services/frontend-v3/src/components/profileForms/primaryInfoForm/PrimaryInfoFormView.jsx
+++ services/frontend-v3/src/components/profileForms/primaryInfoForm/PrimaryInfoFormView.jsx
@@ -10,8 +10,14 @@ import {
   Input,
   Button,
   message,
+  Popover,
 } from "antd";
-import { LinkOutlined, RightOutlined, CheckOutlined } from "@ant-design/icons";
+import {
+  LinkOutlined,
+  RightOutlined,
+  CheckOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
 import { FormattedMessage, injectIntl } from "react-intl";
 import axios from "axios";
 import _ from "lodash";
@@ -22,6 +28,7 @@ import {
   IntlPropType,
   HistoryPropType,
 } from "../../../customPropTypes";
+import FormLabelTooltip from "../../formLabelTooltip/FormLabelTooltip";
 import config from "../../../config";
 import handleError from "../../../functions/handleError";
 
@@ -96,6 +103,13 @@ const PrimaryInfoFormView = ({
       fontWeight: "normal",
       fontStyle: "italic",
       opacity: 0.5,
+    },
+    gedsInfoLink: {
+      display: "inline",
+      float: "right",
+    },
+    popoverStyle: {
+      maxWidth: "430px",
     },
   };
 
@@ -274,6 +288,23 @@ const PrimaryInfoFormView = ({
       return (
         <Title level={2} style={styles.formTitle}>
           2. <FormattedMessage id="setup.primary.information" />
+          <div style={styles.gedsInfoLink}>
+            <Popover
+              trigger="click"
+              tabIndex="0"
+              content={
+                <div style={styles.popoverStyle}>
+                  <FormattedMessage id="profile.geds.edit.info1" />
+                  <a href="https://userprofile.prod.prv/icpup.asp?lang=E">
+                    <FormattedMessage id="profile.geds.edit.info.link" />
+                  </a>{" "}
+                  <FormattedMessage id="profile.geds.edit.info2" />
+                </div>
+              }
+            >
+              <QuestionCircleOutlined />
+            </Popover>
+          </div>
         </Title>
       );
     }
