f6853655109411b704e5605b8d7d789d5e8a7792
Benoit Jeaurond <benoit@jeaurond.dev>
2020-07-03 11:45:05 -0400

    Added console.log

--- services/backend/src/core/search/util/fuzzySearch.js
+++ services/backend/src/core/search/util/fuzzySearch.js
@@ -38,6 +38,8 @@ async function fuzzySearch(profiles, searchValue) {
     ],
   };
 
+  console.log(profiles);
+
   const fuse = new Fuse(profiles, options);
   const results = fuse.search(searchValue).map(({ item }) => item);
   return results;
