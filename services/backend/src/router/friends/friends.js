d9c2bddec1dd49c6190d683f99d63acebd8d4434
Mohamed Radwan <21133129+MohamedRadwan@users.noreply.github.com>
2020-07-03 10:36:26 -0400

    vaildation

--- services/backend/src/router/friends/friends.js
+++ services/backend/src/router/friends/friends.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const { keycloak } = require("../../auth/keycloak");
 const friends = require("../../core/friends/friends");
+const { UUIDValidator } = require("./validator");
 
 const friendsRouter = Router();
 
 friendsRouter
   .route("/:id")
-  .get(keycloak.protect(), friends.getFriendById)
-  .post(keycloak.protect(), friends.addFriend)
-  .delete(keycloak.protect(), friends.removeFriend);
+  .get(keycloak.protect(), [UUIDValidator], friends.getFriendById)
+  .post(keycloak.protect(), [UUIDValidator], friends.addFriend)
+  .delete(keycloak.protect(), [UUIDValidator], friends.removeFriend);
 
 module.exports = friendsRouter;
