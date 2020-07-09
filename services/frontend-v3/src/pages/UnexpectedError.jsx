bc69b80568bf8eb59b091197c7375c492f43f514
Benoit Jeaurond <benoit@jeaurond.dev>
2020-07-09 16:17:14 -0400

    Made error screens centered

--- services/frontend-v3/src/pages/UnexpectedError.jsx
+++ services/frontend-v3/src/pages/UnexpectedError.jsx
@@ -17,51 +17,60 @@ const UnexpectedError = () => {
   };
 
   return (
-    <Result
-      title={<FormattedMessage id="error.redirect.title" />}
-      subTitle={<FormattedMessage id="error.redirect.subtitle" />}
-      status={500}
-      extra={
-        <div style={{ textAlign: "center" }}>
-          <div>
-            <Space size="small">
-              <Button type="primary" onClick={() => history.goBack()}>
-                <FormattedMessage id="error.retry" />
-              </Button>
-              <Button onClick={() => history.push("/")}>
-                <FormattedMessage id="back.to.landing" />
-              </Button>
-              <Button onClick={() => setShowError((oldValue) => !oldValue)}>
-                <FormattedMessage
-                  id={showError ? "error.hide" : "error.show"}
-                />
-              </Button>
-            </Space>
-          </div>
-          <div style={{ display: "inline-block", textAlign: "left" }}>
-            <div style={styles.content}>
-              {showError ? (
-                <List
-                  dataSource={errors}
-                  renderItem={(item) => (
-                    <List.Item>
-                      <List.Item.Meta
-                        title={
-                          <span style={styles.errorTitle}>{item.title}</span>
-                        }
-                        description={item.description.map((val) => (
-                          <p style={styles.errorDescription}>{val}</p>
-                        ))}
-                      />
-                    </List.Item>
-                  )}
-                />
-              ) : null}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Result
+        title={<FormattedMessage id="error.redirect.title" />}
+        subTitle={<FormattedMessage id="error.redirect.subtitle" />}
+        status={500}
+        extra={
+          <div style={{ textAlign: "center" }}>
+            <div>
+              <Space size="small">
+                <Button type="primary" onClick={() => history.goBack()}>
+                  <FormattedMessage id="error.retry" />
+                </Button>
+                <Button onClick={() => history.push("/")}>
+                  <FormattedMessage id="back.to.landing" />
+                </Button>
+                <Button onClick={() => setShowError((oldValue) => !oldValue)}>
+                  <FormattedMessage
+                    id={showError ? "error.hide" : "error.show"}
+                  />
+                </Button>
+              </Space>
+            </div>
+            <div style={{ display: "inline-block", textAlign: "left" }}>
+              <div style={styles.content}>
+                {showError ? (
+                  <List
+                    dataSource={errors}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <List.Item.Meta
+                          title={
+                            <span style={styles.errorTitle}>{item.title}</span>
+                          }
+                          description={item.description.map((val) => (
+                            <p style={styles.errorDescription}>{val}</p>
+                          ))}
+                        />
+                      </List.Item>
+                    )}
+                  />
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
-      }
-    />
+        }
+      />
+    </div>
   );
 };
 
