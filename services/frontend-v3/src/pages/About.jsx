d0241eacd566e46bd6905704ccdfe196e0593ad9
KateAThornley <ktott56789@gmail.com>
2020-07-09 14:28:29 -0400

    setup abt

---
+++ services/frontend-v3/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AboutLayout from "../components/layouts/aboutLayout/AboutLayout";
+
+/** UI for the landing route layout */
+const About = () => {
+  return <AboutLayout />;
+};
+
+export default About;
