70788634f1a46514b0fdaef098f33a58408b7ff7
Benoit Jeaurond <benoit@jeaurond.dev>
2020-07-13 14:11:23 -0400

    Created new results card

--- services/frontend-v3/src/components/resultsCard/ResultsCardView.jsx
+++ services/frontend-v3/src/components/resultsCard/ResultsCardView.jsx
@@ -11,19 +11,33 @@ import {
   Typography,
   Empty,
   Skeleton,
+  Badge,
+  Tooltip,
+  Button,
 } from "antd";
+import {
+  UserAddOutlined,
+  UserDeleteOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
 import { HistoryPropType, ProfileInfoPropType } from "../../customPropTypes";
 import prepareInfo from "../../functions/prepareInfo";
 
 const { Meta } = Card;
 const { Text } = Typography;
 
-const ResultsCardView = ({ history, results, locale, loading }) => {
+const ResultsCardView = ({
+  history,
+  results,
+  locale,
+  loading,
+  userId,
+  connections,
+}) => {
   const styles = {
     smallP: {
-      lineHeight: "4px",
-      zIndex: "-1",
-      marginTop: "10px",
+      margin: 0,
+      marginTop: -10,
     },
   };
 
@@ -32,7 +46,6 @@ const ResultsCardView = ({ history, results, locale, loading }) => {
    *
    * handle how to process when enter key is hit when focusing on a results card
    */
-
   const handleKeyPress = (e, person) => {
     if (e.charCode === 32 || e.charCode === 13) {
       e.preventDefault();
@@ -41,61 +54,136 @@ const ResultsCardView = ({ history, results, locale, loading }) => {
   };
 
   const renderCard = (person, key) => {
+    const actions = [];
+    const isConnection = connections.includes(person.id);
+    let isFriend = true;
+
+    if (person.id !== userId) {
+      actions.push(
+        <Button
+          tabIndex="0"
+          type="link"
+          block
+          icon={
+            isConnection ? (
+              <UserDeleteOutlined style={{ fontSize: 16, marginRight: 5 }} />
+            ) : (
+              <UserAddOutlined style={{ fontSize: 16, marginRight: 5 }} />
+            )
+          }
+        >
+          {isConnection ? (
+            <FormattedMessage id="search.results.cards.add.connection" />
+          ) : (
+            <FormattedMessage id="search.results.cards.remove.connection" />
+          )}
+        </Button>
+      );
+    } else {
+      isFriend = false;
+    }
+
     return (
-      <Col span={6} style={{ height: "100%" }} key={key}>
+      <Col span={24} xxl={12} key={key}>
         <Card
           tabIndex="0"
-          style={{ height: "100%", overflowX: "hidden" }}
+          style={{
+            height: "100%",
+            overflowX: "hidden",
+            display: "flex",
+            flexDirection: "column",
+          }}
           size="small"
           hoverable
           bordered
           onClick={() => history.push(`/secured/profile/${person.id}`)}
           onKeyPress={(e) => handleKeyPress(e, person)}
+          title={person.branch ? <Text>{person.branch}</Text> : undefined}
+          extra={
+            person.groupLevel && person.groupLevel.name ? (
+              <Text>{`${person.groupLevel.name}`}</Text>
+            ) : undefined
+          }
+          actions={actions}
+          bodyStyle={{ flex: 1 }}
         >
-          <Meta
-            avatar={
-              <Avatar
-                size={48}
-                style={{
-                  backgroundColor: person.avatarColor,
-                }}
-              >
-                <Text style={{ fontSize: "25px", color: "white" }}>
-                  {person.nameInitials}
-                </Text>
-              </Avatar>
-            }
-            title={`${person.firstName} ${person.lastName}`}
-            description={<p style={styles.smallP}>{person.jobTitle}</p>}
-          />
+          <Row>
+            <Col>
+              <Row style={{ paddingTop: 4 }}>
+                <Meta
+                  avatar={
+                    <Tooltip
+                      align={{ offset: [18, -3] }}
+                      title={
+                        isFriend
+                          ? `You're a connection of ${person.firstName}`
+                          : undefined
+                      }
+                    >
+                      <Badge
+                        count={
+                          isFriend ? (
+                            <TeamOutlined style={{ color: "#F5F5F5" }} />
+                          ) : undefined
+                        }
+                        offset={[-6, 6]}
+                        style={{
+                          backgroundColor: "#565656",
+                          borderRadius: 20,
+                          padding: 4,
+                        }}
+                      >
+                        <Avatar
+                          size={48}
+                          style={{
+                            backgroundColor: person.avatarColor,
+                          }}
+                        >
+                          <Text style={{ fontSize: "25px", color: "white" }}>
+                            {person.nameInitials}
+                          </Text>
+                        </Avatar>
+                      </Badge>
+                    </Tooltip>
+                  }
+                  title={`${person.firstName} ${person.lastName}`}
+                  description={
+                    <p style={styles.smallP}>
+                      {person.jobTitle ? person.jobTitle : "-"}
+                    </p>
+                  }
+                  style={{ margin: 0, marginRight: 20 }}
+                />
+              </Row>
+            </Col>
 
-          <p style={styles.smallP}>{person.branch}</p>
-          {person.classification &&
-          person.classification.description !== null ? (
-            <p style={styles.smallP}>
-              {`Classification: ${person.classification.description}`}
-            </p>
-          ) : (
-            <p />
-          )}
+            {person.resultSkills.length > 0 && (
+              <Divider
+                type="vertical"
+                style={{ height: "auto", marginRight: 20, marginTop: 4 }}
+              />
+            )}
 
-          <Divider
-            className="results-card-divider"
-            style={styles.divider}
-            orientation="left"
-          >
-            <FormattedMessage id="advanced.search.form.skills" />
-          </Divider>
-
-          {person.resultSkills.map(({ id, name }) => (
-            <Tag
-              color="#004441"
-              style={{ marginBottom: "2px", marginTop: "2px" }}
-              key={id}
-            >
-              {name}
-            </Tag>
-          ))}
+            <Col flex="1">
+              {person.resultSkills.length > 0 && (
+                <Row align="middle" style={{ height: "100%" }} type="flex">
+                  {person.resultSkills.map(({ id, name }) => (
+                    <Col>
+                      <Tag
+                        style={{
+                          marginBottom: "2px",
+                          marginTop: "2px",
+                        }}
+                        key={id}
+                      >
+                        {name}
+                      </Tag>
+                    </Col>
+                  ))}
+                </Row>
+              )}
+            </Col>
+          </Row>
         </Card>
       </Col>
     );
@@ -114,14 +202,14 @@ const ResultsCardView = ({ history, results, locale, loading }) => {
   };
 
   return (
-    <div>
+    <div style={{ padding: "0 12px", maxWidth: 1500 }}>
       {loading && (
         <Card>
           <Skeleton />
         </Card>
       )}
       <Row
-        gutter={[32, 32]}
+        gutter={[8, 8]}
         type="flex"
         justify="left"
         align={results.length === 0 ? "center" : "top"}
@@ -137,6 +225,8 @@ ResultsCardView.propTypes = {
   results: PropTypes.arrayOf(ProfileInfoPropType),
   locale: PropTypes.oneOf(["FRENCH", "ENGLISH"]).isRequired,
   loading: PropTypes.bool.isRequired,
+  userId: PropTypes.string.isRequired,
+  connections: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 ResultsCardView.defaultProps = {
