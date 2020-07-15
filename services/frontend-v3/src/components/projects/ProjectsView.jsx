19a4a80897f9ba21869afc88631eefce4bed6a89
KateAThornley <ktott56789@gmail.com>
2020-07-15 14:51:20 -0400

    added clear Empty messages to profile cards

--- services/frontend-v3/src/components/projects/ProjectsView.jsx
+++ services/frontend-v3/src/components/projects/ProjectsView.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Row, Col, List } from "antd";
+import { FormattedMessage } from "react-intl";
+import { Row, Col, List, Empty } from "antd";
 
 const ProjectsView = ({ projectsInfo }) => {
+  if (projectsInfo.length > 0) {
+    return (
+      <Row>
+        <Col xs={24} lg={24}>
+          <List
+            size="small"
+            itemLayout="horizontal"
+            dataSource={projectsInfo}
+            renderItem={(item) => <List.Item>{item}</List.Item>}
+          />
+        </Col>
+      </Row>
+    );
+  }
   return (
-    <Row>
-      <Col xs={24} lg={24}>
-        <List
-          size="small"
-          itemLayout="horizontal"
-          dataSource={projectsInfo}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
-        />
-      </Col>
-    </Row>
+    <Empty
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+      description={<FormattedMessage id="profile.projects.empty" />}
+    />
   );
 };
 
