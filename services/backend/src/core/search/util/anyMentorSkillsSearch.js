5c0166ef730e65e2176c5f6f84853cb18f5e767f
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2020-07-24 12:26:23 -0400

    Update services/backend/src/core/search/util/anyMentorSkillsSearch.js
    Co-authored-by: Benoît Jeaurond <benoit@jeaurond.dev>

--- services/backend/src/core/search/util/anyMentorSkillsSearch.js
+++ services/backend/src/core/search/util/anyMentorSkillsSearch.js
@@ -2,7 +2,7 @@ async function anyMentorshipSkillSearch(profiles) {
   const skillProf = [];
 
   profiles.forEach((profile) => {
-    if (profile.mentorshipSkills && profile.mentorshipSkills.length) {
+    if (profile.mentorshipSkills && profile.mentorshipSkills.length > 0) {
       skillProf.push(profile);
     }
   });
