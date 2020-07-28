5a35a651be24dadd29f4c8eda953b13379a35180
Mohamed Radwan <21133129+MohamedRadwan@users.noreply.github.com>
2020-07-28 19:51:53 -0400

    update wrapper

--- services/frontend-v3/src/App.jsx
+++ services/frontend-v3/src/App.jsx
@@ -1,92 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Provider as ReduxProvider, useSelector } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { KeycloakProvider } from "@react-keycloak/web";
-import { IntlProvider } from "react-intl";
-import moment from "moment";
-import messagesEn from "./i18n/en_CA.json";
-import messagesFr from "./i18n/fr_CA.json";
-import "moment/locale/en-ca";
-import "moment/locale/fr-ca";
-import AppLayout from "./components/layouts/appLayout/AppLayout";
-import keycloak from "./auth/keycloak";
-import keycloakConfig from "./utils/keycloakConfig";
+import React from "react";
 import Routes from "./routes/Routes";
-
+import AppProvider from "./utils/AppProvider";
 import "./App.css";
 
-import store, { persistor } from "./redux";
-import {
-  setUserId,
-  setUserAvatarColor,
-  setUserEmail,
-  setUserName,
-  setUserInitials,
-} from "./redux/slices/userSlice";
-
-const i18nConfigBuilder = (locale) => ({
-  messages: locale === "ENGLISH" ? messagesEn : messagesFr,
-  formats: {
-    number: {
-      CAD: {
-        style: "currency",
-        currency: "USD",
-        currencyDisplay: "symbol",
-      },
-    },
-  },
-});
-
-const App = () => {
-  const { locale } = useSelector((state) => state.settings);
-  const [i18nConfig, setI18nConfig] = useState(i18nConfigBuilder("en"));
-
-  useEffect(() => {
-    setI18nConfig(i18nConfigBuilder(locale));
-    moment.locale(`${locale === "ENGLISH" ? "en" : "fr"}-ca`);
-
-    // This statement should be temporary, and be removed in the future
-    if (localStorage.getItem("userId")) {
-      const attributes = ["userId", "color", "email", "name", "acronym"];
-      const reduxFunctions = [
-        setUserId,
-        setUserAvatarColor,
-        setUserEmail,
-        setUserName,
-        setUserInitials,
-      ];
-
-      // Moves the info from localStorage to redux and clears it
-      attributes.forEach((attribute, key) => {
-        store.dispatch(reduxFunctions[key](localStorage.getItem(attribute)));
-        localStorage.removeItem(attribute);
-      });
-    }
-  }, [locale]);
-
-  return (
-    <IntlProvider
-      locale={locale === "ENGLISH" ? "en" : "fr"}
-      messages={i18nConfig.messages}
-      formats={i18nConfig.formats}
-    >
-      <Routes />
-    </IntlProvider>
-  );
-};
-
-const ReduxWrappedApp = () => (
-  <KeycloakProvider
-    keycloak={keycloak}
-    initConfig={keycloakConfig}
-    LoadingComponent={() => <AppLayout loading />}
-  >
-    <ReduxProvider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </ReduxProvider>
-  </KeycloakProvider>
+const App = () => (
+  <AppProvider>
+    <Routes />
+  </AppProvider>
 );
 
-export default ReduxWrappedApp;
+export default App;
