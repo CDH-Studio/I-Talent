1495e2c78830e8ebe6a22953ea11fac1b297cbd9
Benoit Jeaurond <benoit@jeaurond.dev>
2020-08-07 16:13:56 -0400

    Add profile not exist option

--- services/frontend-v3/src/components/errorResult/errorResult.jsx
+++ services/frontend-v3/src/components/errorResult/errorResult.jsx
@@ -3,11 +3,15 @@ import { Button } from "antd";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
-import { HomeOutlined } from "@ant-design/icons";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { useSelector } from "react-redux";
 import ErrorResultView from "./errorResultView";
+import AppLayout from "../layouts/appLayout/AppLayout";
 
 const ErrorResult = ({ errorCode }) => {
   const [back, setBack] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const { id } = useSelector((state) => state.user);
 
   const handleClick = () => {
     setBack(true);
@@ -36,16 +40,43 @@ const ErrorResult = ({ errorCode }) => {
         </Button>
       ),
     },
+    profileNotExist: {
+      status: "403",
+      title: <FormattedMessage id="profile.not.found" />,
+      subTitle: <FormattedMessage id="profile.not.found.description" />,
+      extra: (
+        <>
+          <Button onClick={handleClick} type="primary">
+            <HomeOutlined style={{ marginRight: 10 }} />
+            <FormattedMessage id="error.button" />
+          </Button>
+          <Button onClick={() => setProfile(true)}>
+            <UserOutlined style={{ marginRight: 10 }} />
+            <FormattedMessage id="setup.done.view.profile" />
+          </Button>
+        </>
+      ),
+    },
   };
 
   if (back) {
     return <Redirect to="/" />;
   }
-  return <ErrorResultView resultProps={propMap[errorCode]} />;
+
+  if (profile) {
+    return <Redirect to={`/profile/${id}`} />;
+  }
+
+  return (
+    <AppLayout>
+      <ErrorResultView resultProps={propMap[errorCode]} />
+    </AppLayout>
+  );
 };
 
 ErrorResult.propTypes = {
-  errorCode: PropTypes.number.isRequired,
+  errorCode: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    .isRequired,
 };
 
 export default ErrorResult;
