fe45599f95bcad966f45f33887a977fd3e41cf8b
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2020-08-10 08:13:51 -0400

    remove accidental comment

--- services/frontend-v3/src/components/descriptionCard/DescriptionCardView.jsx
+++ services/frontend-v3/src/components/descriptionCard/DescriptionCardView.jsx
@@ -4,13 +4,6 @@ import PropTypes from "prop-types";
 import DescriptionText from "../descriptionText/DescriptionText";
 
 const DescriptionView = ({ data }) => {
-  /**
-   * Generate Competencies Tag List
-   *
-   * Generate a list of Competency Tags
-   * If no competencies are found for the profile then display friendly message
-   */
-
   return <DescriptionText text={data.description} expandable={false} />;
 };
 
