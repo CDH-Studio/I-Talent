da18d3c9d154a4c3cd8922607b9f039c758d9a24
Benoit Jeaurond <benoit@jeaurond.dev>
2020-08-12 14:52:19 -0400

    Updated yarn cache path

--- services/frontend-v3/scripts/install.sh
+++ services/frontend-v3/scripts/install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Used to fix yarn cache warning in openshift
 if [[ ! -z $NODE_ENV ]] && [[ $NODE_ENV = 'production' ]]; then
-  mkdir -p $YARN_CACHE_FOLDER;
   yarn install --production;
 else
   yarn install;
