a354d989979390f0195247ff6b8936a7771ce60d
Benoît Jeaurond <benoit@jeaurond.dev>
2020-08-12 10:46:41 -0400

    [Fix] Make <Select /> search less strict (#753)
    * Created general filter/search function
    
    * Updated Select components to use the filter func
    
    * Fixed bug with office location search

---
+++ services/frontend-v3/src/functions/filterSelectInput.js
@@ -0,0 +1,27 @@
+/**
+ * Helps filtering search inputs while disregarding accents and casing
+ * https://stackoverflow.com/a/37511463/11006837
+ *
+ * @param {string} input Search input value
+ * @param {Object} option Option data from ant design
+ *
+ * @returns {boolean} Returns trus if the input is found in the option data
+ */
+export default (input, option) => {
+  let optionText = option.children;
+
+  if (Array.isArray(optionText)) {
+    optionText = optionText.join("");
+  }
+
+  return optionText
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .includes(
+      input
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+    );
+};
