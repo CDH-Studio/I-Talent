457689f10ab33d9b8c79ef4bdebcb40a3ab2152a
Benoit Jeaurond <benoit@jeaurond.dev>
2020-08-17 09:45:29 -0400

    Cleaned up client's server

--- services/frontend-v3/src/server.jsx
+++ services/frontend-v3/src/server.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { StaticRouter } from "react-router-dom";
 import express from "express";
 import { renderToString } from "react-dom/server";
 import serialize from "serialize-javascript";
@@ -15,39 +14,45 @@ server
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get("/*", (req, res) => {
     const context = {};
-    const markup = renderToString(
-      // <StaticRouter location={req.url}>
-        <App />
-      // </StaticRouter>
-    );
+    const markup = renderToString(<App />);
 
     if (context.url) {
       res.redirect(context.url);
     } else {
       res.status(200).send(
         `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ""
-        }
-        ${
-          process.env.NODE_ENV === "production"
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-        <script>window.env = ${serialize(runtimeConfig)};</script>
-    </body>
-</html>`
+          <html lang="">
+          <head>
+              <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+              <meta charset="utf-8" />
+              <style>
+                body {
+                  background-color: #f0f2f5 !important;
+                }
+              </style>
+              <title>I-Talent</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <meta
+                name="description"
+                content="Search tool to find people based on experience and skills"
+              />
+              ${
+                assets.client.css
+                  ? `<link rel="stylesheet" href="${assets.client.css}">`
+                  : ""
+              }
+              ${
+                process.env.NODE_ENV === "production"
+                  ? `<script src="${assets.client.js}" defer></script>`
+                  : `<script src="${assets.client.js}" defer crossorigin></script>`
+              }
+          </head>
+          <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+              <div id="root">${markup}</div>
+              <script>window.env = ${serialize(runtimeConfig)};</script>
+          </body>
+        </html>`
       );
     }
   });
