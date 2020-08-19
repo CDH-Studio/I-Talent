1c67b4a2187b26c8a4af310499856902d1f9b66f
Benoît Jeaurond <benoit@jeaurond.dev>
2020-08-19 15:38:27 -0400

    Added basic API request logger (#795)

--- services/backend/src/server.js
+++ services/backend/src/server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const timeout = require("connect-timeout");
 const cors = require("cors");
+const morgan = require("morgan");
+
 const { keycloak, sessionInstance } = require("./auth/keycloak");
 const router = require("./router/router");
 const swaggerOptions = require("./docs/swaggerOptions");
@@ -26,6 +28,7 @@ app.use(timeout("5s"));
 app.use(keycloak.middleware());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 app.use("/api", router);
 app.get("/oauth2-redirect.html", function (req, res) {
   res.sendfile("src/docs/oauth2-redirect.html");
