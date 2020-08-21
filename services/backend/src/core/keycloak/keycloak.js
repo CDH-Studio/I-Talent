b7e206cfba734a391c8f443b3b92573265ef3d2d
Benoit Jeaurond <benoit@jeaurond.dev>
2020-08-21 09:13:06 -0400

    Fixed eslint backend

--- services/backend/src/core/keycloak/keycloak.js
+++ services/backend/src/core/keycloak/keycloak.js
@@ -4,53 +4,45 @@ const config = require("../../config");
 const { handleAxiosErrors } = require("../../utils/handleErrors");
 
 const getAccessToken = async () => {
-  try {
-    const data = qs.stringify({
-      grant_type: "client_credentials",
-      client_secret: config.KEYCLOAK_SECRET,
-      client_id: "upskill-api",
-    });
+  const data = qs.stringify({
+    grant_type: "client_credentials",
+    client_secret: config.KEYCLOAK_SECRET,
+    client_id: "upskill-api",
+  });
 
-    const response = await axios({
-      method: "post",
-      baseURL: config.KEYCLOAK_AUTH_SERVER_URL,
-      url: "/realms/individual/protocol/openid-connect/token",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      data: data,
-      timeout: 2000,
-    });
+  const response = await axios({
+    method: "post",
+    baseURL: config.KEYCLOAK_AUTH_SERVER_URL,
+    url: "/realms/individual/protocol/openid-connect/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: data,
+    timeout: 2000,
+  });
 
-    return response.data.access_token;
-  } catch (error) {
-    handleAxiosErrors(error);
-  }
+  return response.data.access_token;
 };
 
 const getGroupIds = async (accessToken) => {
-  try {
-    const groups = await axios({
-      method: "get",
-      baseURL: config.KEYCLOAK_AUTH_SERVER_URL,
-      url: "/admin/realms/individual/groups?search=upskill",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      timeout: 2000,
-    });
+  const groups = await axios({
+    method: "get",
+    baseURL: config.KEYCLOAK_AUTH_SERVER_URL,
+    url: "/admin/realms/individual/groups?search=upskill",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    timeout: 2000,
+  });
 
-    const groupIds = groups.data[0].subGroups.map(({ id, name }) => {
-      return {
-        id,
-        name,
-      };
-    });
+  const groupIds = groups.data[0].subGroups.map(({ id, name }) => {
+    return {
+      id,
+      name,
+    };
+  });
 
-    return groupIds;
-  } catch (error) {
-    handleAxiosErrors(error);
-  }
+  return groupIds;
 };
 
 const filterName = (name) => {
@@ -66,24 +58,20 @@ const filterName = (name) => {
 };
 
 const getMembers = async (accessToken, id, name) => {
-  try {
-    const members = await axios({
-      method: "get",
-      baseURL: config.KEYCLOAK_AUTH_SERVER_URL,
-      url: `/admin/realms/individual/groups/${id}/members`,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      timeout: 2000,
-    });
+  const members = await axios({
+    method: "get",
+    baseURL: config.KEYCLOAK_AUTH_SERVER_URL,
+    url: `/admin/realms/individual/groups/${id}/members`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    timeout: 2000,
+  });
 
-    return {
-      name: filterName(name),
-      ids: members.data.map((member) => member.id),
-    };
-  } catch (error) {
-    handleAxiosErrors(error);
-  }
+  return {
+    name: filterName(name),
+    ids: members.data.map((member) => member.id),
+  };
 };
 
 const getUsers = async (request, response) => {
@@ -102,7 +90,7 @@ const getUsers = async (request, response) => {
 
     response.status(200).send(cleanedData);
   } catch (error) {
-    console.log(error);
+    handleAxiosErrors(error);
     response.status(500);
   }
 };
