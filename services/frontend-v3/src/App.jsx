256c7ffe186386072fe3a5f0c56ba37809ec99b9
Benoit Jeaurond <benoit@jeaurond.dev>
2020-10-02 00:05:54 -0400

    Remove and.less import

--- services/frontend-v3/src/App.jsx
+++ services/frontend-v3/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Routes from "./routes/Routes";
 import "./App.scss";
 import "./index.css";
-import "antd/dist/antd.less";
 import AppProvider from "./utils/AppProvider";
 
 const App = () => (
