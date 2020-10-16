b1f0fb858f4297f7c3c9e5a5cb4bd41fc842926f
Ali Nouri <dynamic11@gmail.com>
2020-10-16 10:30:10 -0400

    add redirect on cancel

--- services/frontend-v3/src/components/privacyModal/PrivacyModal.jsx
+++ services/frontend-v3/src/components/privacyModal/PrivacyModal.jsx
@@ -3,20 +3,26 @@ import PrivacyModalView from "./PrivacyModalView";
 import { useSelector, useDispatch } from "react-redux";
 import { setIsPrivacyAccepted } from "../../redux/slices/userSlice";
 import { useKeycloak } from "@react-keycloak/web";
+import { useHistory } from "react-router-dom";
 
 const PrivacyModal = ({ data }) => {
+  const history = useHistory();
   const dispatch = useDispatch();
   const { isPrivacyAccepted } = useSelector((state) => state.user);
   const [keycloak] = useKeycloak();
 
-  const handleOk = (e) => {
+  const handleOk = () => {
     dispatch(setIsPrivacyAccepted(true));
+  };
+
+  const handleCancel = () => {
+    history.push("/logout");
   };
   return (
     <PrivacyModalView
       showModal={!isPrivacyAccepted}
       handleOk={handleOk}
-      handleCancel={handleOk}
+      handleCancel={handleCancel}
       keycloak={keycloak}
     />
   );
