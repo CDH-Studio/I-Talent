332a8aadd6d64931506014a2f723f20679434247
Benoit Jeaurond <benoit@jeaurond.dev>
2020-10-18 23:55:00 -0400

    Revert merge change

--- services/frontend-v3/src/components/layouts/appLayout/footer/FooterView.jsx
+++ services/frontend-v3/src/components/layouts/appLayout/footer/FooterView.jsx
@@ -7,32 +7,34 @@ import { Link } from "react-router-dom";
 const { Footer } = Layout;
 const { Text } = Typography;
 
-const FooterView = () => (
-  <Footer className="footer">
-    <div className="aroundLinksSmall">
-      <Link className="link" to="/about">
-        <FormattedMessage id="footer.about.link" />
-      </Link>
+const FooterView = () => {
+  return (
+    <Footer className="footer">
+      <div className="aroundLinksSmall">
+        <Link className="link" to="/about">
+          <FormattedMessage id="footer.about.link" />
+        </Link>
 
-      <Text className="dashes">|</Text>
+        <Text className="dashes">|</Text>
 
-      <Link className="link" to="/help">
-        <FormattedMessage id="footer.contact.link" />
-      </Link>
+        <Link className="link" to="/help">
+          <FormattedMessage id="footer.contact.link" />
+        </Link>
 
-      <Text className="dashes">|</Text>
+        <Text className="dashes">|</Text>
 
-      <Link className="link" to="/terms">
-        <FormattedMessage id="footer.terms.and.conditions.link" />
-      </Link>
+        <Link className="link" to="/terms">
+          <FormattedMessage id="footer.terms.and.conditions.link" />
+        </Link>
 
-      <Text className="dashes">|</Text>
+        <Text className="dashes">|</Text>
 
-      <Link className="link" to="/privacy">
-        <FormattedMessage id="footer.privacy.link" />
-      </Link>
-    </div>
-  </Footer>
-);
+        <Link className="link" to="/privacy">
+          <FormattedMessage id="footer.privacy.link" />
+        </Link>
+      </div>
+    </Footer>
+  );
+};
 
 export default FooterView;
