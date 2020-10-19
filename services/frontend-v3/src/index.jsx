b70906650439ec1b73d8d51944bd84d799a7dad1
Benoit Jeaurond <benoit@jeaurond.dev>
2020-10-19 09:23:29 -0400

    Seperated styling files

--- services/frontend-v3/src/index.jsx
+++ services/frontend-v3/src/index.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
-
-import "./index.css";
-import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
