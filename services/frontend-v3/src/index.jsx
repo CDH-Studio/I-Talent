a690a3d21803d695d128c62b8b98363b9fe71192
Ali Nouri <dynamic11@gmail.com>
2020-10-20 11:39:48 -0400

    fix IE bug
    polyfill has to be defined before App

--- services/frontend-v3/src/index.jsx
+++ services/frontend-v3/src/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
