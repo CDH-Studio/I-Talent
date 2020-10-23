7e8f1643e7ddd3ce7b438d30043ad7596d2acea1
Ali Nouri <dynamic11@gmail.com>
2020-10-23 11:01:49 -0400

    improve ex feeder test

--- services/frontend-v3/src/components/exFeeder/ExFeeder.test.jsx
+++ services/frontend-v3/src/components/exFeeder/ExFeeder.test.jsx
@@ -3,20 +3,26 @@ import { render } from "@testing-library/react";
 
 import ExFeederCard from "./ExFeeder";
 
-test("Test render of edit menu", () => {
-  const { getByText } = render(<ExFeeder data={data} editableCardBool={true} />;);
+const data = {
+  exfeeder: true,
+};
 
-  <ExFeeder data={data} editableCardBool={true} />;
-  getByText("Test Title");
-});
+// test("Test render of edit menu", () => {
+//   const { getByText } = render(<ExFeeder data={data} editableCardBool={true} />;);
+
+//   <ExFeeder data={data} editableCardBool={true} />;
+//   getByText("Test Title");
+// });
 
 test("Test header with just main text", async () => {
-  const { getByText } = render(<Header title="Test Title" />);
-  getByText("Test Title");
+  const { getByText } = render(
+    <ExFeederCard data={data} editableCardBool={false} />
+  );
+  getByText("I am nominated in the EX-feeder program");
 });
 
-test("Test header with main text and extra content", async () => {
-  const { getByText } = render(<Header title="Test Title" extra="extra" />);
-  getByText("Test Title");
-  getByText("extra");
-});
+// test("Test header with main text and extra content", async () => {
+//   const { getByText } = render(<Header title="Test Title" extra="extra" />);
+//   getByText("Test Title");
+//   getByText("extra");
+// });
