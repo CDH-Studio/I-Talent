ecca2f25e5098031c9148c99bed7bfd2a328bad4
Ali Nouri <dynamic11@gmail.com>
2020-10-23 16:45:35 -0400

    try to get react intl to work

--- services/frontend-v3/src/components/exFeeder/ExFeeder.test.jsx
+++ services/frontend-v3/src/components/exFeeder/ExFeeder.test.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { IntlProvider } from "react-intl";
 import { render } from "@testing-library/react";
 
-import ExFeederCard from "./ExFeeder";
+//import ExFeederCard from "./ExFeeder";
+import AppProvider from "../../utils/AppProvider";
 
 const data = {
-  exfeeder: true,
+  exFeeder: true,
+  visibleCards: {
+    exFeeder: true,
+  },
 };
+
+// const i18nConfigBuilder = (locale) => ({
+//   messages: locale === "ENGLISH" ? messagesEn : messagesFr,
+//   formats: {
+//     number: {
+//       CAD: {
+//         style: "currency",
+//         currency: "USD",
+//         currencyDisplay: "symbol",
+//       },
+//     },
+//   },
+// });
+
+// PropTypes.bool,
+// PropTypes.oneOf(["PRIVATE", "CONNECTIONS", "PUBLIC"]),
 
 // test("Test render of edit menu", () => {
 //   const { getByText } = render(<ExFeeder data={data} editableCardBool={true} />;);
@@ -16,7 +37,16 @@ const data = {
 
 test("Test header with just main text", async () => {
   const { getByText } = render(
-    <ExFeederCard data={data} editableCardBool={false} />
+    // <IntlProvider
+    //   locale={"en"}
+    //   messages={i18nConfig.messages}
+    //   formats={i18nConfig.formats}
+    // >
+    <AppProvider>
+      <ExFeederCard data={data} editableCardBool={false} />
+    </AppProvider>
+
+    // </IntlProvider>
   );
   getByText("I am nominated in the EX-feeder program");
 });
