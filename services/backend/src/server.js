af1160b3e23862c310f7be0cb62c6daf374a1de1
Mohamed Radwan <21133129+MohamedRadwan@users.noreply.github.com>
2020-10-28 12:25:06 -0400

    update to charjs

--- services/backend/src/server.js
+++ services/backend/src/server.js
@@ -14,24 +14,16 @@ const config = require("./config");
 const app = express();
 
 app.set("trust proxy", true);
-app.use(cors());
-app.use(helmet());
-app.use(sessionInstance);
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Authorization, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
-});
-app.use(timeout("5s"));
-app.use(keycloak.middleware());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(morgan(config.ENV === "development" ? "dev" : "combined"));
+app.use([
+  cors(),
+  helmet(),
+  sessionInstance,
+  timeout("5s"),
+  keycloak.middleware(),
+  bodyParser.urlencoded({ extended: true }),
+  bodyParser.json(),
+  morgan(config.ENV === "development" ? "dev" : "combined"),
+]);
 app.use("/api", router);
 app.get("/oauth2-redirect.html", function (req, res) {
   res.sendfile("src/docs/oauth2-redirect.html");
