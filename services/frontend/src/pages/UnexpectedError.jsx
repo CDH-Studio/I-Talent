9fd586a466a5c26e23ddc5c098c341dbacf3316a
Benoit Jeaurond <benoit@jeaurond.dev>
2020-10-28 21:47:37 -0400

    Fix table of errors

--- services/frontend/src/pages/UnexpectedError.jsx
+++ services/frontend/src/pages/UnexpectedError.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, List, Result, Space } from "antd";
+import { Button, List, Result, Space, Col, Row } from "antd";
 import { useSelector } from "react-redux";
 import { FormattedMessage } from "react-intl";
 import { useHistory } from "react-router-dom";
@@ -17,9 +17,8 @@ const UnexpectedError = () => {
   const history = useHistory();
 
   const styles = {
-    content: { textAlign: "left", maxWidth: "1200px", margin: "0px" },
-    errorTitle: {},
-    errorDescription: { marginBottom: "0.0em", marginLeft: "1em" },
+    list: { width: "100%", marginTop: "1em", textAlign: "left", maxWidth: 700 },
+    errorDescription: { marginBottom: 0 },
   };
 
   return (
@@ -29,21 +28,25 @@ const UnexpectedError = () => {
         subTitle={<FormattedMessage id="error.redirect.subtitle" />}
         status={500}
         extra={
-          <div style={{ textAlign: "center" }}>
-            <div>
-              <Space size="small">
+          <Col>
+            <Row justify="center" gutter={[10, 10]}>
+              <Col>
                 <Button type="primary" onClick={() => history.goBack()}>
                   <ReloadOutlined />
                   <span>
                     <FormattedMessage id="error.retry" />
                   </span>
                 </Button>
+              </Col>
+              <Col>
                 <Button onClick={() => history.push("/")}>
                   <HomeOutlined />
                   <span>
                     <FormattedMessage id="back.to.landing" />
                   </span>
                 </Button>
+              </Col>
+              <Col>
                 <Button onClick={() => setShowError((oldValue) => !oldValue)}>
                   <ExclamationCircleOutlined />
                   <span>
@@ -52,30 +55,25 @@ const UnexpectedError = () => {
                     />
                   </span>
                 </Button>
-              </Space>
-            </div>
-            <div style={{ display: "inline-block", textAlign: "left" }}>
-              <div style={styles.content}>
-                {showError ? (
-                  <List
-                    dataSource={errors}
-                    renderItem={(item) => (
-                      <List.Item>
-                        <List.Item.Meta
-                          title={
-                            <span style={styles.errorTitle}>{item.title}</span>
-                          }
-                          description={item.description.map((val) => (
-                            <p style={styles.errorDescription}>{val}</p>
-                          ))}
-                        />
-                      </List.Item>
-                    )}
-                  />
-                ) : null}
-              </div>
-            </div>
-          </div>
+              </Col>
+            </Row>
+            <Row justify="center">
+              {showError && <List
+                dataSource={errors}
+                style={styles.list}
+                renderItem={(item) => (
+                  <List.Item>
+                    <List.Item.Meta
+                      title={item.title}
+                      description={item.description.map((val) => (
+                        <p style={styles.errorDescription}>{val}</p>
+                      ))}
+                    />
+                  </List.Item>
+                )}
+              />}
+            </Row>
+          </Col>
         }
       />
     </AppLayout>
