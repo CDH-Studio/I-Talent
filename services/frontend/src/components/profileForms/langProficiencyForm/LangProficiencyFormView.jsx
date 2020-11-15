ecdc954273f3a2df2259f0c0250745f959bbf6c2
Benoît Jeaurond <benoit@jeaurond.dev>
2020-11-15 13:22:05 -0500

    [Fix] Update deps - Minimize react-bundle (#1045)
    * Potentially fix IE
    
    * Cleanup babel
    
    * Made antd icons a bit smaller
    
    * made moment js smaller
    
    * Removed manual tree shaking
    
    * Slim lodash
    
    * Updated deps, reverted react upgrade
    
    * Revert terser webpack plugin update
    
    * Don't show footer when loading (users can click)
    
    * Updated packages
    
    * Fix linter
    
    * Fix lodash problem with tables

--- services/frontend/src/components/profileForms/langProficiencyForm/LangProficiencyFormView.jsx
+++ services/frontend/src/components/profileForms/langProficiencyForm/LangProficiencyFormView.jsx
@@ -16,9 +16,7 @@ import {
 import { InfoCircleOutlined } from "@ant-design/icons";
 import { FormattedMessage, injectIntl } from "react-intl";
 import dayjs from "dayjs";
-import isEqual from "lodash/isEqual";
-import identity from "lodash/identity";
-import pickBy from "lodash/pickBy";
+import { isEqual, identity, pickBy } from "lodash";
 import PropTypes from "prop-types";
 import { useSelector, useDispatch } from "react-redux";
 import { Prompt } from "react-router";
