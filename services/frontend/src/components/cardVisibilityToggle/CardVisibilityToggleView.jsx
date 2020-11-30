408dc8618e49b5499b582aff4a4bfc22fa626be3
Mohamed Radwan <21133129+MohamedRadwan@users.noreply.github.com>
2020-11-30 12:48:32 -0500

    [Fix] Clean up Qualifications styling  (#1112)
    * fix styling
    
    * update description
    
    * delete description component

--- services/frontend/src/components/cardVisibilityToggle/CardVisibilityToggleView.jsx
+++ services/frontend/src/components/cardVisibilityToggle/CardVisibilityToggleView.jsx
@@ -7,7 +7,7 @@ import {
 } from "@ant-design/icons";
 import { FormattedMessage, useIntl } from "react-intl";
 import PropTypes from "prop-types";
-import "./CardVisibilityToggle.less";
+import "./CardVisibilityToggleView.less";
 
 const { Option } = Select;
 
