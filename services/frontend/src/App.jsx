bb46c4d2cf2ef3dcd90ed20e85c695cb3dfb3931
Mohamed Radwan <21133129+MohamedRadwan@users.noreply.github.com>
2020-12-02 12:21:07 -0500

    Update App.jsx

--- services/frontend/src/App.jsx
+++ services/frontend/src/App.jsx
@@ -4,7 +4,7 @@ import AppProvider from "./utils/AppProvider";
 import "./styling/accessibility.less";
 import "./styling/custom_antd.less";
 import "./styling/index.less";
-
+// test
 const App = () => (
   <AppProvider>
     <Routes />
