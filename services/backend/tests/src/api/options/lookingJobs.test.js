5b27ab4bda10db3d1a2bfb5bfc9fe75356afa3cd
Benoit Jeaurond <benoit@jeaurond.dev>
2020-12-05 15:54:06 -0500

    Added test for lookingJobs/secClearance/attachment

---
+++ services/backend/tests/src/api/options/lookingJobs.test.js
@@ -0,0 +1,136 @@
+const request = require("supertest");
+const { getBearerToken } = require("../../../mocks");
+
+const path = "/api/option/lookingJobs";
+
+describe(`GET ${path}`, () => {
+  beforeEach(() => console.log.mockClear());
+
+  describe("when not authenticated", () => {
+    test("should not process request - 403", async () => {
+      const res = await request(app).get(path);
+
+      expect(res.statusCode).toBe(403);
+      expect(res.text).toBe("Access denied");
+      expect(console.log).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("when authenticated", () => {
+    const data = [
+      [
+        "ENGLISH",
+        [
+          {
+            opLookingJobId: 1,
+            description: "z",
+          },
+          {
+            opLookingJobId: 2,
+            description: "B",
+          },
+        ],
+        [
+          {
+            id: 2,
+            description: "B",
+          },
+          {
+            id: 1,
+            description: "z",
+          },
+        ],
+      ],
+      [
+        "FRENCH",
+        [
+          {
+            opLookingJobId: 3,
+            description: "b",
+          },
+        ],
+        [
+          {
+            id: 3,
+            description: "b",
+          },
+        ],
+      ],
+    ];
+
+    describe.each(data)("in %s", (language, prismaData, result) => {
+      let res;
+
+      beforeAll(async () => {
+        prisma.opTransLookingJob.findMany.mockResolvedValue(prismaData);
+
+        res = await request(app)
+          .get(`${path}?language=${language}`)
+          .set("Authorization", getBearerToken());
+      });
+
+      afterAll(() => {
+        prisma.opTransLookingJob.findMany.mockClear();
+      });
+
+      test("should process request - 200", () => {
+        expect(res.statusCode).toBe(200);
+        expect(console.log).not.toHaveBeenCalled();
+      });
+
+      test("should call prisma with specified params", () => {
+        expect(prisma.opTransLookingJob.findMany).toHaveBeenCalledWith({
+          where: {
+            language,
+          },
+          select: {
+            opLookingJobId: true,
+            description: true,
+          },
+          orderBy: {
+            description: "asc",
+          },
+        });
+      });
+
+      test("should return expected result", () => {
+        expect(res.body).toStrictEqual(result);
+      });
+
+      test("should trigger error if there's a database problem - 500", async () => {
+        prisma.opTransLookingJob.findMany.mockRejectedValue(new Error());
+
+        const dbRes = await request(app)
+          .get(`${path}?language=${language}`)
+          .set("Authorization", getBearerToken());
+
+        expect(dbRes.statusCode).toBe(500);
+        expect(dbRes.text).toBe("Internal Server Error");
+        expect(console.log).toHaveBeenCalled();
+        expect(prisma.opTransLookingJob.findMany).toHaveBeenCalled();
+
+        prisma.opTransLookingJob.findMany.mockClear();
+      });
+    });
+
+    test("should throw validation error without language query param - 422", async () => {
+      const res = await request(app)
+        .get(path)
+        .set("Authorization", getBearerToken());
+
+      expect(res.statusCode).toBe(422);
+      expect(console.log).toHaveBeenCalled();
+      expect(prisma.opTransLookingJob.findMany).not.toHaveBeenCalled();
+    });
+
+    test("should throw validation error invalid language query param - 422", async () => {
+      const res = await request(app)
+        .get(`${path}?language=asdfafse`)
+        .set("Authorization", getBearerToken());
+
+      expect(res.statusCode).toBe(422);
+      expect(console.log).toHaveBeenCalled();
+      expect(prisma.opTransLookingJob.findMany).not.toHaveBeenCalled();
+    });
+  });
+});
