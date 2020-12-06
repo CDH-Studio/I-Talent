24f33ed017bded18a7b5e47599d1da2c24cb17a5
Benoit Jeaurond <benoit@jeaurond.dev>
2020-12-05 21:10:10 -0500

    Added rest of topFiveX tests

--- services/backend/tests/src/api/stats/topFiveCompetencies.test.js
+++ services/backend/tests/src/api/stats/topFiveCompetencies.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const faker = require("faker");
 const { getBearerToken } = require("../../../mocks");
 
 const path = "/api/stats/topFiveCompetencies";
@@ -15,45 +16,162 @@ describe(`GET ${path}`, () => {
   });
 
   describe("when authenticated", () => {
-    test.todo("should process request in English - 200");
-    test.todo("should process request in French - 200");
+    describe("when doing a normal query", () => {
+      const data = [
+        [
+          "ENGLISH",
+          [
+            { id: faker.random.uuid(), competencyId: 1 },
+            { id: faker.random.uuid(), competencyId: 1 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 3 },
+            { id: faker.random.uuid(), competencyId: 3 },
+            { id: faker.random.uuid(), competencyId: 4 },
+            { id: faker.random.uuid(), competencyId: 4 },
+            { id: faker.random.uuid(), competencyId: 4 },
+            { id: faker.random.uuid(), competencyId: 5 },
+            { id: faker.random.uuid(), competencyId: 6 },
+          ],
+          [
+            { opCompetencyId: 1, name: "z" },
+            { opCompetencyId: 2, name: "b" },
+            { opCompetencyId: 3, name: "a" },
+            { opCompetencyId: 4, name: "c" },
+            { opCompetencyId: 5, name: "d" },
+          ],
+          [
+            { count: 4, name: "b" },
+            { count: 3, name: "c" },
+            { count: 2, name: "a" },
+            { count: 2, name: "z" },
+            { count: 1, name: "d" },
+          ],
+          [2, 4, 1, 3, 5],
+        ],
+        [
+          "FRENCH",
+          [
+            { id: faker.random.uuid(), competencyId: 1 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 3 },
+            { id: faker.random.uuid(), competencyId: 3 },
+          ],
+          [
+            { opCompetencyId: 1, name: "a" },
+            { opCompetencyId: 2, name: "b" },
+            { opCompetencyId: 3, name: "c" },
+          ],
+          [
+            { count: 3, name: "b" },
+            { count: 2, name: "c" },
+            { count: 1, name: "a" },
+          ],
+          [2, 3, 1],
+        ],
+      ];
 
-    test("should throw validation error without language query param - 422", async () => {
-      const res = await request(app)
-        .get(path)
-        .set("Authorization", getBearerToken());
+      describe.each(data)(
+        "in %s",
+        (
+          language,
+          prismaCompetencyData,
+          prismaOpCompetencyData,
+          result,
+          topFiveCompetencyIds
+        ) => {
+          let res;
 
-      expect(res.statusCode).toBe(422);
-      expect(console.log).toHaveBeenCalled();
+          beforeAll(async () => {
+            prisma.competency.findMany.mockResolvedValue(prismaCompetencyData);
+            prisma.opTransCompetency.findMany.mockResolvedValue(
+              prismaOpCompetencyData
+            );
 
-      console.log.mockClear();
+            res = await request(app)
+              .get(`${path}?language=${language}`)
+              .set("Authorization", getBearerToken());
+          });
+
+          afterAll(() => {
+            prisma.competency.findMany.mockClear();
+            prisma.opTransCompetency.findMany.mockClear();
+          });
+
+          test("should process request - 200", () => {
+            expect(res.statusCode).toBe(200);
+            expect(console.log).not.toHaveBeenCalled();
+          });
+
+          test("should call prisma with specified params", () => {
+            expect(prisma.competency.findMany).toHaveBeenCalledWith({
+              select: {
+                id: true,
+                competencyId: true,
+              },
+            });
+
+            expect(prisma.opTransCompetency.findMany).toHaveBeenCalledWith({
+              where: {
+                opCompetencyId: {
+                  in: topFiveCompetencyIds,
+                },
+                language,
+              },
+              select: {
+                name: true,
+                opCompetencyId: true,
+              },
+            });
+          });
+
+          test("should return expected result", () => {
+            expect(res.body).toStrictEqual(result);
+          });
+        }
+      );
+
+      test("should trigger error if there's a database problem - 500", async () => {
+        prisma.competency.findMany.mockRejectedValue(new Error());
+
+        const res = await request(app)
+          .get(`${path}?language=ENGLISH`)
+          .set("Authorization", getBearerToken());
+
+        expect(res.statusCode).toBe(500);
+        expect(res.text).toBe("Internal Server Error");
+        expect(console.log).toHaveBeenCalled();
+        expect(prisma.competency.findMany).toHaveBeenCalled();
+
+        prisma.competency.findMany.mockClear();
+        console.log.mockClear();
+      });
     });
+  });
 
-    test("should throw validation error invalid language query param - 422", async () => {
-      const res = await request(app)
-        .get(`${path}?language=asdasasf`)
-        .set("Authorization", getBearerToken());
+  test("should throw validation error without language query param - 422", async () => {
+    const res = await request(app)
+      .get(path)
+      .set("Authorization", getBearerToken());
 
-      expect(res.statusCode).toBe(422);
-      expect(console.log).toHaveBeenCalled();
+    expect(res.statusCode).toBe(422);
+    expect(console.log).toHaveBeenCalled();
 
-      console.log.mockClear();
-    });
+    console.log.mockClear();
+  });
 
-    test("should trigger error if there's a database problem - 500", async () => {
-      prisma.competency.findMany.mockRejectedValue(new Error());
+  test("should throw validation error invalid language query param - 422", async () => {
+    const res = await request(app)
+      .get(`${path}?language=asdasasf`)
+      .set("Authorization", getBearerToken());
 
-      const res = await request(app)
-        .get(`${path}?language=ENGLISH`)
-        .set("Authorization", getBearerToken());
+    expect(res.statusCode).toBe(422);
+    expect(console.log).toHaveBeenCalled();
 
-      expect(res.statusCode).toBe(500);
-      expect(res.text).toBe("Internal Server Error");
-      expect(console.log).toHaveBeenCalled();
-      expect(prisma.competency.findMany).toHaveBeenCalled();
-
-      prisma.competency.findMany.mockClear();
-      console.log.mockClear();
-    });
+    console.log.mockClear();
   });
 });
--- services/backend/tests/src/api/stats/topFiveDevelopmentalGoals.test.js
+++ services/backend/tests/src/api/stats/topFiveDevelopmentalGoals.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const faker = require("faker");
 const { getBearerToken } = require("../../../mocks");
 
 const path = "/api/stats/topFiveDevelopmentalGoals";
@@ -15,8 +16,183 @@ describe(`GET ${path}`, () => {
   });
 
   describe("when authenticated", () => {
-    test.todo("should process request in English - 200");
-    test.todo("should process request in French - 200");
+    describe("when doing a normal query", () => {
+      const data = [
+        [
+          "ENGLISH",
+          [
+            { id: faker.random.uuid(), skillId: 1 },
+            { id: faker.random.uuid(), skillId: 1 },
+            { id: faker.random.uuid(), skillId: 2 },
+            { id: faker.random.uuid(), skillId: 2 },
+            { id: faker.random.uuid(), skillId: 2 },
+            { id: faker.random.uuid(), skillId: 3 },
+          ],
+          [
+            { id: faker.random.uuid(), competencyId: 1 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 2 },
+            { id: faker.random.uuid(), competencyId: 3 },
+            { id: faker.random.uuid(), competencyId: 3 },
+          ],
+          [
+            { opSkillId: 1, name: "z" },
+            { opSkillId: 2, name: "b" },
+            { opSkillId: 3, name: "a" },
+          ],
+          [
+            { opCompetencyId: 1, name: "c" },
+            { opCompetencyId: 2, name: "d" },
+            { opCompetencyId: 3, name: "e" },
+          ],
+          [
+            { count: 4, name: "d" },
+            { count: 3, name: "b" },
+            { count: 2, name: "e" },
+            { count: 2, name: "z" },
+            { count: 1, name: "a" },
+          ],
+          [2, 1, 3],
+          [2, 3, 1],
+        ],
+        [
+          "FRENCH",
+          [
+            { id: faker.random.uuid(), skillId: 1 },
+            { id: faker.random.uuid(), skillId: 2 },
+            { id: faker.random.uuid(), skillId: 2 },
+            { id: faker.random.uuid(), skillId: 2 },
+            { id: faker.random.uuid(), skillId: 3 },
+            { id: faker.random.uuid(), skillId: 3 },
+          ],
+          [],
+          [
+            { opSkillId: 1, name: "a" },
+            { opSkillId: 2, name: "b" },
+            { opSkillId: 3, name: "c" },
+          ],
+          [],
+          [
+            { count: 3, name: "b" },
+            { count: 2, name: "c" },
+            { count: 1, name: "a" },
+          ],
+          [2, 3, 1],
+          [],
+        ],
+      ];
+
+      describe.each(data)(
+        "in %s",
+        (
+          language,
+          prismaDevSkillData,
+          prismaDevCompData,
+          prismaOpSkillData,
+          prismaOpCompetencyData,
+          result,
+          topFiveSkillIds,
+          topFiveCompetencyIds
+        ) => {
+          let res;
+
+          beforeAll(async () => {
+            prisma.developmentalGoal.findMany
+              .mockReturnValueOnce(prismaDevCompData)
+              .mockReturnValueOnce(prismaDevSkillData);
+            prisma.opTransSkill.findMany.mockResolvedValue(prismaOpSkillData);
+            prisma.opTransCompetency.findMany.mockResolvedValue(
+              prismaOpCompetencyData
+            );
+
+            res = await request(app)
+              .get(`${path}?language=${language}`)
+              .set("Authorization", getBearerToken());
+          });
+
+          afterAll(() => {
+            prisma.developmentalGoal.findMany.mockClear();
+            prisma.opTransSkill.findMany.mockClear();
+            prisma.opTransCompetency.findMany.mockClear();
+          });
+
+          test("should process request - 200", () => {
+            expect(res.statusCode).toBe(200);
+            expect(console.log).not.toHaveBeenCalled();
+          });
+
+          test("should call prisma with specified params", () => {
+            expect(prisma.developmentalGoal.findMany).toHaveBeenCalledWith({
+              where: {
+                skillId: null,
+              },
+              select: {
+                id: true,
+                competencyId: true,
+              },
+            });
+
+            expect(prisma.developmentalGoal.findMany).toHaveBeenCalledWith({
+              where: {
+                competencyId: null,
+              },
+              select: {
+                id: true,
+                skillId: true,
+              },
+            });
+
+            expect(prisma.opTransCompetency.findMany).toHaveBeenCalledWith({
+              where: {
+                opCompetencyId: {
+                  in: topFiveCompetencyIds,
+                },
+                language,
+              },
+              select: {
+                name: true,
+                opCompetencyId: true,
+              },
+            });
+
+            expect(prisma.opTransSkill.findMany).toHaveBeenCalledWith({
+              where: {
+                opSkillId: {
+                  in: topFiveSkillIds,
+                },
+                language,
+              },
+              select: {
+                name: true,
+                opSkillId: true,
+              },
+            });
+          });
+
+          test("should return expected result", () => {
+            expect(res.body).toStrictEqual(result);
+          });
+        }
+      );
+
+      test("should trigger error if there's a database problem - 500", async () => {
+        prisma.developmentalGoal.findMany.mockRejectedValue(new Error());
+
+        const res = await request(app)
+          .get(`${path}?language=ENGLISH`)
+          .set("Authorization", getBearerToken());
+
+        expect(res.statusCode).toBe(500);
+        expect(res.text).toBe("Internal Server Error");
+        expect(console.log).toHaveBeenCalled();
+        expect(prisma.developmentalGoal.findMany).toHaveBeenCalled();
+
+        prisma.developmentalGoal.findMany.mockClear();
+        console.log.mockClear();
+      });
+    });
 
     test("should throw validation error without language query param - 422", async () => {
       const res = await request(app)
@@ -37,22 +213,6 @@ describe(`GET ${path}`, () => {
       expect(res.statusCode).toBe(422);
       expect(console.log).toHaveBeenCalled();
 
-      console.log.mockClear();
-    });
-
-    test("should trigger error if there's a database problem - 500", async () => {
-      prisma.developmentalGoal.findMany.mockRejectedValue(new Error());
-
-      const res = await request(app)
-        .get(`${path}?language=ENGLISH`)
-        .set("Authorization", getBearerToken());
-
-      expect(res.statusCode).toBe(500);
-      expect(res.text).toBe("Internal Server Error");
-      expect(console.log).toHaveBeenCalled();
-      expect(prisma.developmentalGoal.findMany).toHaveBeenCalled();
-
-      prisma.developmentalGoal.findMany.mockClear();
       console.log.mockClear();
     });
   });
