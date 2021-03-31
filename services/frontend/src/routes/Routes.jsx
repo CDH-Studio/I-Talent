f794f927189ef58a45e101fda391fe439ac4ea3b
Mohamed <21133129+MohamedRadwan@users.noreply.github.com>
2021-03-31 11:50:57 -0400

    [New] add drupal pages to italent (#1246)
    * add druoal pages
    
    * Update FooterView.jsx
    
    * update url

--- services/frontend/src/routes/Routes.jsx
+++ services/frontend/src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { Route, Switch } from "react-router-dom";
-import { LandingPage, UnexpectedError, About, Logout } from "../pages";
+import { LandingPage, UnexpectedError, Logout } from "../pages";
 import ErrorNumber from "../components/errorResult/errorNumber";
 import Admin from "./Admin";
 import Secured from "./Secured";
@@ -17,10 +17,6 @@ const Routes = () => (
       />
       <Route exact path="/logout" render={() => <Logout />} />
       <Route path="/admin" render={() => <Admin />} />
-      <Route path="/about" render={() => <About type="about" />} />
-      <Route path="/help" render={() => <About type="help" />} />
-      <Route path="/terms" render={() => <About type="terms" />} />
-      <Route path="/privacy" render={() => <About type="privacy" />} />
       <Route path="/error" render={() => <UnexpectedError />} />
       <Route path="/forbidden" render={() => <ErrorNumber error="403" />} />
       <Route
