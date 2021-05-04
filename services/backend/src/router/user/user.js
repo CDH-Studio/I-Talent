8723948d8a55785d13d3c38e023d446148d7ec81
Mohamed <21133129+MohamedRadwan@users.noreply.github.com>
2021-05-04 16:07:41 -0400

    [Fix] security hotspot (#1334)
    * fix sec hotspot
    
    * fix import

--- services/backend/src/router/user/user.js
+++ services/backend/src/router/user/user.js
@@ -3,7 +3,7 @@ const { keycloak } = require("../../auth/keycloak");
 const user = require("../../core/user/user");
 const { createUserValidator } = require("./validator");
 const { UUIDValidator } = require("../util/commonValidators");
-const { validationMiddlware } = require("../../utils/middlewares");
+const { validationMiddlware } = require("../../utils/middleware");
 
 const userRouter = Router();
 
