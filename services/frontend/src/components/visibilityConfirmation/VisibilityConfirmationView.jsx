beeb7fc98a4bf4f4de136f3aec8f09d43a8e5b86
Rizvi Rab <31704878+Rizvida1@users.noreply.github.com>
2021-05-11 17:14:02 -0400

    [Fix] Modal will close on error (#1368)
    * Modal will close on error
    
    * Fixed prop error

--- services/frontend/src/components/visibilityConfirmation/VisibilityConfirmationView.jsx
+++ services/frontend/src/components/visibilityConfirmation/VisibilityConfirmationView.jsx
@@ -30,11 +30,16 @@ const VisibilityConfirmationView = ({
     }
   };
 
+  const onClickOk = () => {
+    onOk();
+    onCloseModal();
+  };
+
   return (
     <Modal
       title={<FormattedMessage id="visibility.review.title" />}
       visible={visible}
-      onOk={onOk}
+      onOk={onClickOk}
       onCancel={onCloseModal}
       width="70%"
     >
@@ -95,8 +100,12 @@ VisibilityConfirmationView.propTypes = {
     PropTypes.oneOf(["PRIVATE", "CONNECTIONS", "PUBLIC"])
   ).isRequired,
   visible: PropTypes.bool.isRequired,
-  onOk: PropTypes.func.isRequired,
+  onOk: PropTypes.func,
   onCloseModal: PropTypes.func.isRequired,
+};
+
+VisibilityConfirmationView.defaultProps = {
+  onOk: () => {},
 };
 
 export default VisibilityConfirmationView;
