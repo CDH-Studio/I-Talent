e9dd065a7f4ed10fc043c2e4235095cfaf666a7a
Ali Nouri <dynamic11@gmail.com>
2021-06-21 14:58:01 -0400

    translation and clean up

--- services/frontend/src/components/layouts/appLayout/skipToContent/SkipToContentView.jsx
+++ services/frontend/src/components/layouts/appLayout/skipToContent/SkipToContentView.jsx
@@ -1,10 +1,11 @@
 import { Button } from "antd";
 import PropTypes from "prop-types";
+import { FormattedMessage } from "react-intl";
 import "./SkipToContentView.less";
 
 const SkipToContentView = ({ contentId }) => (
   <Button className="skip-to-content-link" href={contentId} tabIndex="0">
-    Skip to content
+    <FormattedMessage id="skip.to.content" />
   </Button>
 );
 
