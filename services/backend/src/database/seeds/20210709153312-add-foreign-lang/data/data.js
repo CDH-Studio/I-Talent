571316350e247ff1eb439ef5a4034c1eb2cf2a8f
MohamedRadwan <21133129+MohamedRadwan@users.noreply.github.com>
2021-07-13 00:12:19 -0400

    add seed values

---
+++ services/backend/src/database/seeds/20210709153312-add-foreign-lang/data/data.js
@@ -0,0 +1,580 @@
+﻿module.exports = [
+  {
+    en: "Foreign Languages",
+    fr: "Langues étrangères",
+    skills: [
+      {
+        en: "Afrikaans",
+        fr: "Afrikaans",
+      },
+      {
+        en: "Albanian",
+        fr: "Albanais(e)",
+      },
+      {
+        en: "American Sign Language (ASL)",
+        fr: "Langue des signes américaine (ASL)",
+      },
+      {
+        en: "Amharic",
+        fr: "Amharique",
+      },
+      {
+        en: "Arabic ",
+        fr: "Arabe",
+      },
+      {
+        en: "Aramaic",
+        fr: "Araméen",
+      },
+      {
+        en: "Armenian",
+        fr: "Arménien(ne)",
+      },
+      {
+        en: "Assamese",
+        fr: "Assamais",
+      },
+      {
+        en: "Aymara",
+        fr: "Aymara",
+      },
+      {
+        en: "Azerbaijani",
+        fr: "Azerbaïdjanais(e)",
+      },
+      {
+        en: "Balochi",
+        fr: "Balochi",
+      },
+      {
+        en: "Bamanankan",
+        fr: "Bamanankaise",
+      },
+      {
+        en: "Bashkort (Bashkir)",
+        fr: "Bashkort (Bashkir)",
+      },
+      {
+        en: "Basque",
+        fr: "Basque",
+      },
+      {
+        en: "Belarusan",
+        fr: "Biélorusse",
+      },
+      {
+        en: "Bengali",
+        fr: "Bengali",
+      },
+      {
+        en: "Bhojpuri",
+        fr: "Bhojpuri",
+      },
+      {
+        en: "Bislama",
+        fr: "Bichlamar",
+      },
+      {
+        en: "Bosnian",
+        fr: "Bosniaque",
+      },
+      {
+        en: "Brahui",
+        fr: "Brahui",
+      },
+      {
+        en: "Bulgarian",
+        fr: "Bulgare",
+      },
+      {
+        en: "Burmese",
+        fr: "Birman(e)",
+      },
+      {
+        en: "Cantonese",
+        fr: "Cantonais",
+      },
+      {
+        en: "Catalan",
+        fr: "Catalan(e)",
+      },
+      {
+        en: "Cebuano",
+        fr: "Cebuano",
+      },
+      {
+        en: "Chechen",
+        fr: "tchétchène",
+      },
+      {
+        en: "Cherokee",
+        fr: "Cherokee",
+      },
+      {
+        en: "Croatian",
+        fr: "Croate",
+      },
+      {
+        en: "Czech",
+        fr: "Tchèque",
+      },
+      {
+        en: "Dakota",
+        fr: "Dakota",
+      },
+      {
+        en: "Danish",
+        fr: "Danois(e)",
+      },
+      {
+        en: "Dari",
+        fr: "Dari",
+      },
+      {
+        en: "Dholuo",
+        fr: "Dholuo",
+      },
+      {
+        en: "Dutch",
+        fr: "Néerlandais(e)",
+      },
+      {
+        en: "Esperanto",
+        fr: "Espéranto",
+      },
+      {
+        en: "Estonian",
+        fr: "Estonien(ne)",
+      },
+      {
+        en: "Éwé",
+        fr: "Éwé",
+      },
+      {
+        en: "Finnish",
+        fr: "Finlandais(e)",
+      },
+      {
+        en: "Georgian",
+        fr: "Géorgien(ne)",
+      },
+      {
+        en: "German",
+        fr: "Allemand(e) ",
+      },
+      {
+        en: "Gikuyu / Kikuyu",
+        fr: "Gikuyu / Kikuyu",
+      },
+      {
+        en: "Greek",
+        fr: "Grec(que)",
+      },
+      {
+        en: "Guarani",
+        fr: "Guarani",
+      },
+      {
+        en: "Gujarati",
+        fr: "Gujarati",
+      },
+      {
+        en: "Haitian Creole",
+        fr: "Créole haïtien",
+      },
+      {
+        en: "Hausa",
+        fr: "Haoussa",
+      },
+      {
+        en: "Hawaiian",
+        fr: "Hawaïen(ne)",
+      },
+      {
+        en: "Hawaiian Creole",
+        fr: "Créole hawaïen",
+      },
+      {
+        en: "Hebrew ",
+        fr: "Hébreu(e)",
+      },
+      {
+        en: "Hiligaynon",
+        fr: "Hiligaynon",
+      },
+      {
+        en: "Hindi",
+        fr: "Hindi",
+      },
+      {
+        en: "Hungarian",
+        fr: "Hongrois(e)",
+      },
+      {
+        en: "Icelandic",
+        fr: "Islandais",
+      },
+      {
+        en: "Igbo",
+        fr: "Igbo",
+      },
+      {
+        en: "Ilocano",
+        fr: "Ilocano",
+      },
+      {
+        en: "Indonesian (Bahasa Indonesia)",
+        fr: "Indonésien(ne)",
+      },
+      {
+        en: "Inuit/Inupiaq",
+        fr: "Inuit/Inupiaq",
+      },
+      {
+        en: "Irish Gaelic",
+        fr: "Gaélique irlandais",
+      },
+      {
+        en: "Italian",
+        fr: "Italien(ne)",
+      },
+      {
+        en: "Japanese",
+        fr: "Japonais(e)",
+      },
+      {
+        en: "Jarai",
+        fr: "Jarai",
+      },
+      {
+        en: "Javanese",
+        fr: "Javanais",
+      },
+      {
+        en: "K’iche’",
+        fr: "Quiché",
+      },
+      {
+        en: "Kabyle",
+        fr: "Kabyle",
+      },
+      {
+        en: "Kannada",
+        fr: "Kannada",
+      },
+      {
+        en: "Kashmiri",
+        fr: "Cachemire",
+      },
+      {
+        en: "Kazakh",
+        fr: "Kazakh(e)",
+      },
+      {
+        en: "Khmer",
+        fr: "Khmer",
+      },
+      {
+        en: "Khoekhoe",
+        fr: "Khoekhoe",
+      },
+      {
+        en: "Korean",
+        fr: "Coréen(ne)",
+      },
+      {
+        en: "Kurdish",
+        fr: "Kurde",
+      },
+      {
+        en: "Kyrgyz",
+        fr: "Kirghiz(e)",
+      },
+      {
+        en: "Lao",
+        fr: "Laotien",
+      },
+      {
+        en: "Latin",
+        fr: "Latin(e)",
+      },
+      {
+        en: "Latvian",
+        fr: "Lettonie",
+      },
+      {
+        en: "Lingala",
+        fr: "Lingala",
+      },
+      {
+        en: "Lithuanian",
+        fr: "Lituanien(ne)",
+      },
+      {
+        en: "Macedonian",
+        fr: "Macédonien",
+      },
+      {
+        en: "Maithili",
+        fr: "Maithili",
+      },
+      {
+        en: "Malagasy",
+        fr: "Malgache",
+      },
+      {
+        en: "Malay (Bahasa Melayu)",
+        fr: "Malais(e)",
+      },
+      {
+        en: "Malayalam",
+        fr: "Malayalam",
+      },
+      {
+        en: "Mandarin (Chinese)",
+        fr: "Mandarin(e)",
+      },
+      {
+        en: "Marathi",
+        fr: "Marathi",
+      },
+      {
+        en: "Mende",
+        fr: "Mende",
+      },
+      {
+        en: "Mongolian",
+        fr: "Mongol(e)",
+      },
+      {
+        en: "Nahuatl",
+        fr: "Nahuatl",
+      },
+      {
+        en: "Navajo",
+        fr: "Navajo",
+      },
+      {
+        en: "Nepali",
+        fr: "Népalais(e)",
+      },
+      {
+        en: "Norwegian",
+        fr: "Norvégien(ne)",
+      },
+      {
+        en: "Ojibwa",
+        fr: "Ojibwa",
+      },
+      {
+        en: "Oriya",
+        fr: "Oriya",
+      },
+      {
+        en: "Oromo",
+        fr: "Oromo",
+      },
+      {
+        en: "Pashto",
+        fr: "Pashto",
+      },
+      {
+        en: "Persian",
+        fr: "Persan(e)",
+      },
+      {
+        en: "Polish",
+        fr: "Polonais",
+      },
+      {
+        en: "Portuguese",
+        fr: "Portugais",
+      },
+      {
+        en: "Punjabi",
+        fr: "Punjabi",
+      },
+      {
+        en: "Quebec Sign Language (LSQ)",
+        fr: "Langue des signes quebecoise (LSQ)",
+      },
+      {
+        en: "Quechua",
+        fr: "Quechua",
+      },
+      {
+        en: "Romani",
+        fr: "Romani",
+      },
+      {
+        en: "Romanian",
+        fr: "Roumain(e)",
+      },
+      {
+        en: "Russian",
+        fr: "Russe",
+      },
+      {
+        en: "Rwanda",
+        fr: "Rwanda",
+      },
+      {
+        en: "Samoan",
+        fr: "Samoan",
+      },
+      {
+        en: "Sanskrit",
+        fr: "Sanskrit(e)",
+      },
+      {
+        en: "Serbian",
+        fr: "Serbe",
+      },
+      {
+        en: "Shona",
+        fr: "Shona",
+      },
+      {
+        en: "Sindhi",
+        fr: "Sindhi",
+      },
+      {
+        en: "Sinhala",
+        fr: "Cinghalais",
+      },
+      {
+        en: "Slovak",
+        fr: "Slovaque",
+      },
+      {
+        en: "Slovene",
+        fr: "Slovène",
+      },
+      {
+        en: "Somali",
+        fr: "Somali",
+      },
+      {
+        en: "Spanish",
+        fr: "Espagnol(e)",
+      },
+      {
+        en: "Swahili",
+        fr: "Swahili",
+      },
+      {
+        en: "Swedish",
+        fr: "Suédois(e)",
+      },
+      {
+        en: "Tachelhit",
+        fr: "Tachelhit",
+      },
+      {
+        en: "Tagalog",
+        fr: "Tagalog",
+      },
+      {
+        en: "Tajiki",
+        fr: "Tajiki",
+      },
+      {
+        en: "Tamil",
+        fr: "Tamil",
+      },
+      {
+        en: "Tatar",
+        fr: "Tatar(e)",
+      },
+      {
+        en: "Telugu",
+        fr: "Telugu",
+      },
+      {
+        en: "Thai",
+        fr: "Thaïlandais",
+      },
+      {
+        en: "Tibetic Languages",
+        fr: "Langues tibétiques",
+      },
+      {
+        en: "Tigrigna",
+        fr: "Tigrinya",
+      },
+      {
+        en: "Tok Pisin",
+        fr: "Tok Pisin",
+      },
+      {
+        en: "Turkish",
+        fr: "Turc(que)",
+      },
+      {
+        en: "Turkmen",
+        fr: "Turkmène",
+      },
+      {
+        en: "Ukrainian",
+        fr: "Ukrainien(ne)",
+      },
+      {
+        en: "Urdu",
+        fr: "Ourdou",
+      },
+      {
+        en: "Uyghur",
+        fr: "Ouïghur",
+      },
+      {
+        en: "Uzbek",
+        fr: "Ouzbek",
+      },
+      {
+        en: "Vietnamese",
+        fr: "Vietnamien(ne)",
+      },
+      {
+        en: "Warlpiri",
+        fr: "Warlbiri",
+      },
+      {
+        en: "Welsh",
+        fr: "Gallois",
+      },
+      {
+        en: "Wolof",
+        fr: "Wolof(e)",
+      },
+      {
+        en: "Xhosa",
+        fr: "Xhosa",
+      },
+      {
+        en: "Yakut",
+        fr: "Yakut",
+      },
+      {
+        en: "Yiddish",
+        fr: "Yiddish",
+      },
+      {
+        en: "Yoruba",
+        fr: "Yoruba",
+      },
+      {
+        en: "Yucatec",
+        fr: "Yucatec",
+      },
+      {
+        en: "Zapotec",
+        fr: "Zapotèque",
+      },
+      {
+        en: "Zulu",
+        fr: "Zoulou",
+      },
+    ],
+  },
+];
--- services/backend/src/database/seeds/README.md
+++ services/backend/src/database/seeds/README.md
@@ -1,14 +1,12 @@
 # I-Talent Seeds
 
-Seed management is done manually (with the `dbSeeds` table in the databse), please follow the following instructions.
+Seed management is done manually (with the `dbSeeds` table in the database), please follow the following instructions.
 
 ## Create a seed
 
 If you want to create a new seed:
 
-1.  Create a folder here with the current date/time alongside with a brief explanation of the purspose of the seed
-
-- (ex: if the date is June 10th 2020, the time is 3:03:05 PM (include seconds also), and the message is "Added more fake users", the folder name would be `20200610150305-added-more-fake-users`)
+1. Create a folder here with the current date/time alongside with a brief explanation of the purpose of the seed - (ex: if the date is June 10th 2020, the time is 3:03:05 PM (include seconds also), and the message is "Added more fake users", the folder name would be `20200610150305-added-more-fake-users`)
 
 2. Create a `data` folder inside of it where you data would be
 3. Create a `index.js` where you would make the prisma calls to add data to the database
@@ -20,22 +18,22 @@ const path = require("path");
 const prisma = require("../..");
 const data = require("./data");
 
-async function seedData() {
-  const setupData = []; // Add prisma api calls to this array
-  return Promise.all(setupData);
+async function seedStaticInfo() {
+  const staticInfo = []; // add the prisma function here
+  return Promise.all(staticInfo);
 }
 
 async function seed() {
   const folderName = path.dirname(__filename).split(path.sep).pop();
 
-  const dbSeed = await prisma.dbSeeds.findOne({
+  const dbSeed = await prisma.dbSeed.findUnique({
     where: { id: folderName },
   });
 
-  if (dbSeed) {
+  if (!dbSeed) {
     console.log(`---- Starting seeding: ${folderName} ----`);
-    await seedData();
-    await prisma.dbSeeds.create({
+    await seedStaticInfo();
+    await prisma.dbSeed.create({
       data: {
         id: folderName,
       },
