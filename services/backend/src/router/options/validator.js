91cd9ac19622e27d0d7e3bcccc5a0f0f48702058
MohamedRadwan <21133129+MohamedRadwan@users.noreply.github.com>
2021-08-18 12:02:35 -0400

    remove unused import

--- services/backend/src/router/options/validator.js
+++ services/backend/src/router/options/validator.js
@@ -1,4 +1,4 @@
-const { body, query, check } = require("express-validator");
+const { body, query } = require("express-validator");
 const { isUUID } = require("validator");
 
 const deleteManyValidator = [
