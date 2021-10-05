aa595d12cdb8ca7b741624087e7bc4ac0694f100
Ali Nouri <dynamic11@gmail.com>
2021-10-05 09:24:15 -0400

    refactor the developmental goals card

--- services/frontend/src/components/learningDevelopment/LearningDevelopmentView.jsx
+++ services/frontend/src/components/learningDevelopment/LearningDevelopmentView.jsx
@@ -1,62 +1,91 @@
 import { FormattedMessage } from "react-intl";
-import { LinkOutlined } from "@ant-design/icons";
-import { Col, Empty, List, Row, Tag } from "antd";
+import { LinkOutlined, TagTwoTone } from "@ant-design/icons";
+import { Col, Empty, Row, Tag } from "antd";
 import { PropTypes } from "prop-types";
 
 const LearningDevelopmentView = ({ devGoals, devAttachments }) => {
-  const getUrl = (item) =>
-    item.map((i) => (
-      <a href={i.url} rel="noopener noreferrer" target="_blank">
-        <Tag key={i.id} color="#727272" style={{ cursor: "pointer" }}>
-          <LinkOutlined />
-          <span>{i.name.name}</span>
-        </Tag>
-      </a>
-    ));
-
-  const dataSource = [
-    {
-      render: (
-        <>
-          {devGoals.length > 0 ? (
-            <div style={{ marginTop: 7 }}>
-              {Object.values(devGoals).map(({ name, id }) => (
-                <Tag key={id} color="#00605e">
-                  {name}
-                </Tag>
-              ))}
-            </div>
-          ) : (
-            <Empty
-              description={<FormattedMessage id="developmental.goals.empty" />}
-              image={Empty.PRESENTED_IMAGE_SIMPLE}
+  /**
+   * Generate the developmental goal tags for the profile
+   * @param {object} DevelopmentalGoals - Object describing the developmental goals
+   * @param {string} DevelopmentalGoals.id - Unique id for dev goal
+   * @param {string} DevelopmentalGoals.name - Translated name of dev goal
+   */
+  const generateDevelopmentalGoals = (DevelopmentalGoals) =>
+    DevelopmentalGoals &&
+    DevelopmentalGoals.length > 0 && (
+      <Row className="mb-2">
+        <Col>
+          <div className="d-block">
+            <TagTwoTone
+              aria-hidden="true"
+              className="mr-1 d-inline"
+              twoToneColor="#3CBAB3"
             />
-          )}
-        </>
-      ),
-      title: <FormattedMessage id="developmental.goals" />,
-    },
-  ];
-  return (
-    <>
-      <Row>
-        <Col span={24}>
-          <List
-            dataSource={dataSource}
-            itemLayout="horizontal"
-            renderItem={({ title, render }) => (
-              <List.Item>
-                <Col span={24}>
-                  <List.Item.Meta title={title} />
-                  {render}
-                </Col>
-              </List.Item>
-            )}
-          />
+            <h4 className="mt-1 d-inline">
+              <FormattedMessage id="developmental.goals" />:
+            </h4>
+          </div>
+          {DevelopmentalGoals.map((goal) => (
+            <Tag key={goal.name} color="#00605e">
+              {goal.name}
+            </Tag>
+          ))}
         </Col>
       </Row>
-      {getUrl(devAttachments)}
-    </>
+    );
+
+  /**
+   * Generate the supporting document links for the developmental goals
+   * @param {object} SupportingLinks - Object describing the supporting documents
+   * @param {string} SupportingLinks.id - Unique id the document
+   * @param {object} SupportingLinks.name - Object describing of the document type
+   * @param {object} SupportingLinks.name.name - Translated name of the document type
+   * @param {string} SupportingLinks.name.id - Unique id the document type
+   */
+  const generateSupportingLinks = (SupportingLinks) =>
+    SupportingLinks &&
+    SupportingLinks.length > 0 && (
+      <Row className="mb-2">
+        <Col>
+          <div className="d-block">
+            <LinkOutlined
+              aria-hidden="true"
+              className="mr-1 d-inline"
+              style={{ color: "#3CBAB3" }}
+            />
+            <h4 className="mt-1 d-inline">
+              <FormattedMessage id="supporting.document" />:
+            </h4>
+          </div>
+          {SupportingLinks.map((i) => (
+            <a href={i.url} rel="noopener noreferrer" target="_blank">
+              <Tag key={i.id} color="#727272" style={{ cursor: "pointer" }}>
+                <LinkOutlined aria-hidden="true" />
+                <span>{i.name.name}</span>
+              </Tag>
+            </a>
+          ))}
+        </Col>
+      </Row>
+    );
+
+  if (
+    (devGoals && devGoals.length > 0) ||
+    (devAttachments && devAttachments.length > 0)
+  ) {
+    return (
+      <>
+        {generateDevelopmentalGoals(devGoals)}
+        {generateSupportingLinks(devAttachments)}
+      </>
+    );
+  }
+
+  return (
+    <Empty
+      description={<FormattedMessage id="developmental.goals.empty" />}
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+    />
   );
 };
 
