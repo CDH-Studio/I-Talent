a19247b3fb0aabfe3681a3a247c93a5936698224
Ali Nouri <dynamic11@gmail.com>
2021-10-22 10:28:39 -0400

    move functions outside components

--- services/frontend/src/components/experienceCard/ExperienceCard.jsx
+++ services/frontend/src/components/experienceCard/ExperienceCard.jsx
@@ -1,4 +1,6 @@
+import { useMemo } from "react";
 import { useIntl } from "react-intl";
+import { FileDoneOutlined, LinkOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 import PropTypes from "prop-types";
 
@@ -6,72 +8,90 @@ import { ProfileInfoPropType } from "../../utils/customPropTypes";
 import ProfileCards from "../profileCards/ProfileCards";
 import ExperienceCardView from "./ExperienceCardView";
 
+const getExperienceDuration = (startDate, endDate, ongoingDate, intl) => {
+  const formatedStartDate = dayjs(startDate).format("MMMM YYYY");
+  const formatedEndDate = dayjs(endDate).format("MMMM YYYY");
+
+  if (startDate && endDate) {
+    return `${formatedStartDate} - ${formatedEndDate}`;
+  }
+
+  if (ongoingDate && startDate) {
+    return `${formatedStartDate} - ${intl.formatMessage({
+      id: "date.present",
+    })}`;
+  }
+
+  if (ongoingDate) {
+    return intl.formatMessage({
+      id: "experience.date.present",
+    });
+  }
+
+  if (startDate && !endDate) {
+    return formatedStartDate;
+  }
+
+  if (!startDate && endDate) {
+    return formatedEndDate;
+  }
+
+  return "-";
+};
+
+const formatAttachmentLinks = (attachmentLinks) =>
+  attachmentLinks
+    ? attachmentLinks.map((a) => ({
+        href: a.url,
+        icon: <LinkOutlined />,
+        key: a.id,
+        label: a.name.name,
+      }))
+    : [];
+
+const formatProjects = (projects) =>
+  projects
+    ? projects.map((i) => ({
+        icon: <FileDoneOutlined />,
+        key: i,
+        label: i,
+      }))
+    : [];
+
+const getExperienceInfo = (dataSource, intl) => {
+  if (!dataSource || !dataSource.experiences) {
+    return [];
+  }
+
+  return dataSource.experiences.map(
+    ({
+      startDate,
+      endDate,
+      ongoingDate,
+      description,
+      jobTitle,
+      organization,
+      attachmentLinks,
+      projects,
+    }) => ({
+      attachmentLinks: formatAttachmentLinks(attachmentLinks),
+      description,
+      duration: getExperienceDuration(startDate, endDate, ongoingDate, intl),
+      icon: "solution",
+      jobTitle,
+      organization,
+      projects: formatProjects(projects),
+    })
+  );
+};
+
 const ExperienceCard = ({ data, editableCardBool }) => {
   const intl = useIntl();
 
-  const getExperienceDuration = (startDate, endDate, ongoingDate) => {
-    const formatedStartDate = dayjs(startDate).format("MMMM YYYY");
-    const formatedEndDate = dayjs(endDate).format("MMMM YYYY");
-
-    if (startDate && endDate) {
-      return `${formatedStartDate} - ${formatedEndDate}`;
-    }
-
-    if (ongoingDate && startDate) {
-      return `${formatedStartDate} - ${intl.formatMessage({
-        id: "date.present",
-      })}`;
-    }
-
-    if (ongoingDate) {
-      return intl.formatMessage({
-        id: "experience.date.present",
-      });
-    }
-
-    if (startDate && !endDate) {
-      return formatedStartDate;
-    }
-
-    if (!startDate && endDate) {
-      return formatedEndDate;
-    }
-
-    return "-";
-  };
-
-  const getExperienceInfo = (dataSource) => {
-    if (!dataSource || !dataSource.experiences) {
-      return [];
-    }
-
-    return dataSource.experiences.map(
-      ({
-        startDate,
-        endDate,
-        ongoingDate,
-        description,
-        jobTitle,
-        organization,
-        attachmentLinks,
-        projects,
-      }) => ({
-        attachmentLinks: attachmentLinks
-          ? attachmentLinks.map((a) => ({
-              id: a.id,
-              name: a.name.name,
-              url: a.url,
-            }))
-          : [],
-        description,
-        duration: getExperienceDuration(startDate, endDate, ongoingDate),
-        icon: "solution",
-        jobTitle,
-        organization,
-        projects,
-      })
-    );
-  };
+  const formattedExperienceInfo = useMemo(
+    () => getExperienceInfo(data, intl),
+    [data, intl]
+  );
 
   return (
     <ProfileCards
@@ -84,7 +104,7 @@ const ExperienceCard = ({ data, editableCardBool }) => {
       titleString={intl.formatMessage({ id: "experience" })}
       visibility={data.visibleCards.experience}
     >
-      <ExperienceCardView experienceInfo={getExperienceInfo(data)} />
+      <ExperienceCardView experienceInfo={formattedExperienceInfo} />
     </ProfileCards>
   );
 };
--- services/frontend/src/components/experienceCard/ExperienceCardView.jsx
+++ services/frontend/src/components/experienceCard/ExperienceCardView.jsx
@@ -4,91 +4,78 @@ import {
   FileDoneOutlined,
   LinkOutlined,
 } from "@ant-design/icons";
-import { Avatar, Empty, List, Tag } from "antd";
+import { Avatar, Empty, List } from "antd";
 import PropTypes from "prop-types";
+
+import TagList from "../tagList/TagList";
 
 import "./ExperienceCardView.less";
 
-const ExperienceCardView = ({ experienceInfo }) => {
-  /**
-   * Generate styled description text
-   * @param {string} text - text to display as description
-   * @returns {React.ReactElement} - React Element
-   */
-  const generateDescriptionBody = (text) =>
-    text && (
-      <div className="education-descriptionViewText my-2">
-        <h5 className="visually-hidden">
-          <FormattedMessage id="description" />
+/**
+ * Generate styled description text
+ * @param {string} text - text to display as description
+ * @returns {React.ReactElement} - React Element
+ */
+const generateDescriptionBody = (text) =>
+  text && (
+    <div className="education-descriptionViewText my-2">
+      <h5 className="visually-hidden">
+        <FormattedMessage id="description" />
+      </h5>
+      {text}
+    </div>
+  );
+
+/**
+ * Generate the supporting document links for the developmental goals
+ * @param {object} SupportingLinks - Object describing the supporting link
+ * @param {string} SupportingLinks.id - Unique id of the supporting link
+ * @param {string} SupportingLinks.url - URL to the supporting link
+ * @param {string} SupportingLinks.name - Name of the supporting link type
+ * @returns {React.ReactElement} - React Element
+ */
+const generateSupportingLinks = (SupportingLinks) =>
+  SupportingLinks &&
+  SupportingLinks.length > 0 && (
+    <>
+      <div className="d-block">
+        <LinkOutlined
+          aria-hidden="true"
+          className="mr-1 d-inline"
+          style={{ color: "#3CBAB3" }}
+        />
+        <h5 className="mt-1 d-inline">
+          <FormattedMessage id="attachment.links.employment" />
         </h5>
-        {text}
       </div>
-    );
+      <TagList data={SupportingLinks} tagStyle="link" />
+    </>
+  );
 
-  /**
-   * Generate the supporting document links for the developmental goals
-   * @param {object} SupportingLinks - Object describing the supporting link
-   * @param {string} SupportingLinks.id - Unique id of the supporting link
-   * @param {string} SupportingLinks.url - URL to the supporting link
-   * @param {string} SupportingLinks.name - Name of the supporting link type
-   * @returns {React.ReactElement} - React Element
-   */
-  const generateSupportingLinks = (SupportingLinks) =>
-    SupportingLinks &&
-    SupportingLinks.length > 0 && (
-      <>
-        <div className="d-block">
-          <LinkOutlined
-            aria-hidden="true"
-            className="mr-1 d-inline"
-            style={{ color: "#3CBAB3" }}
-          />
-          <h5 className="mt-1 d-inline">
-            <FormattedMessage id="attachment.links.employment" />
-          </h5>
-        </div>
-        {SupportingLinks.map((link) => (
-          <a href={link.url} rel="noopener noreferrer" target="_blank">
-            <Tag key={link.id} color="#727272" style={{ cursor: "pointer" }}>
-              <LinkOutlined aria-hidden="true" className="mr-1" />
-              {link.name}
-              <span className="screenReaderOnly">
-                <FormattedMessage id="opens.in.new.tab" />
-              </span>
-            </Tag>
-          </a>
-        ))}
-      </>
-    );
-
-  /**
-   * Generate the list of projects
-   * @param {Array.<string>} projects - list of project names
-   * @returns {React.ReactElement} - React Element
-   */
-  const generateProjectsList = (projects) =>
-    projects &&
-    projects.length > 0 && (
-      <div className="mb-1">
-        <div className="d-block">
-          <FileDoneOutlined
-            aria-hidden="true"
-            className="mr-1 d-inline"
-            style={{ color: "#3CBAB3" }}
-          />
-          <h5 className="mt-1 d-inline">
-            <FormattedMessage id="projects" />
-          </h5>
-        </div>
-        {projects.map((i) => (
-          <Tag key={i} color="#727272">
-            <FileDoneOutlined aria-hidden="true" className="mr-1" />
-            {i}
-          </Tag>
-        ))}
+/**
+ * Generate the list of projects
+ * @param {Array.<string>} projects - list of project names
+ * @returns {React.ReactElement} - React Element
+ */
+const generateProjectsList = (projects) =>
+  projects &&
+  projects.length > 0 && (
+    <div className="mb-1">
+      <div className="d-block">
+        <FileDoneOutlined
+          aria-hidden="true"
+          className="mr-1 d-inline"
+          style={{ color: "#3CBAB3" }}
+        />
+        <h5 className="mt-1 d-inline">
+          <FormattedMessage id="projects" />
+        </h5>
       </div>
-    );
+      <TagList data={projects} tagStyle="secondary" />
+    </div>
+  );
 
+const ExperienceCardView = ({ experienceInfo }) => {
   if (experienceInfo && experienceInfo.length > 0) {
     return (
       <List
